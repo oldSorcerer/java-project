3b352c47c6016e025c5de9a754f2d765e0bb9dd1
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-03 20:19:48 +0300

    add solution level 02 task 17

---
+++ src/main/java/level02/task17/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level02.task17;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем Scanner для чтения данных с клавиатуры
+
+
+        // Считываем одну строку как пароль
+
+
+        // Выводим введенный пароль на экран
+
+    }
+}
