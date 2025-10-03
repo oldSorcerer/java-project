b071c1d68322a3073b701bb8958826b8f468e1b7
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-03 20:19:58 +0300

    add solution level 02 task 19

---
+++ src/main/java/level02/task19/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level02.task19;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем Scanner для чтения данных с клавиатуры
+
+
+        // Считываем цены двух товаров как целые числа
+
+
+        // Складываем и выводим сумму
+
+    }
+}
