75c47f8550b79a9a1f51a4117d378d5bff81b591
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-03 20:18:52 +0300

    add solution level 02 task 06

---
+++ src/main/java/level02/task06/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level02.task06;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменные и присваиваем им значения
+
+
+        // Объединяем имя и фамилию через пробел
+
+
+        // Выводим получившееся полное имя
+
+    }
+}
