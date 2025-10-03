e0679b1125773033e98136eb9d1a3d9291177fe4
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-03 20:17:21 +0300

    add solution level 01 task 05

---
+++ src/main/java/level01/task05/Solution.java
@@ -0,0 +1,11 @@
+package ru.javarush.java.core.level01.task05;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную типа String с именем myMessage
+
+
+        // Выводим значение переменной myMessage на экран
+
+    }
+}
