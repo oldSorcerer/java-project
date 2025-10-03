d25a5a1af08518505224c4e79bed8267e3c4bbea
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-03 20:19:32 +0300

    add solution level 02 task 14

---
+++ src/main/java/level02/task14/Solution.java
@@ -0,0 +1,18 @@
+package ru.javarush.java.core.level02.task14;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную projectName и задаем ей исходное название проекта
+
+
+        // Объявляем переменную newProjectName и инициализируем ее значением из projectName
+
+
+        // Изменяем значение копии названия проекта
+
+
+        // Выводим на экран оригинальное и измененное название проекта
+
+
+    }
+}
