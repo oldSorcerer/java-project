1c5d968c1f757db51652fb47d2e87734e654de82
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-03 20:19:22 +0300

    add solution level 02 task 12

---
+++ src/main/java/level02/task12/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level02.task12;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Очки приходят как текстовые строки с отрицательными числами
+
+
+        // Преобразуем строки в числа типа int
+
+
+        // Складываем результаты двух раундов
+
+
+        // Выводим общий счёт на экран
+
+    }
+}
