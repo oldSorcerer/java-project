4ea268d8aefafcb9cf4d9a268454a473056327b8
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-03 20:19:53 +0300

    add solution level 02 task 18

---
+++ src/main/java/level02/task18/Solution.java
@@ -0,0 +1,20 @@
+package ru.javarush.java.core.level02.task18;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект Scanner для чтения данных с клавиатуры
+
+
+        // Запрашиваем и считываем имя игрока
+
+
+        // Запрашиваем и считываем возраст игрока
+
+
+        // Сначала выводим возраст, затем имя, каждый с новой строки
+
+
+    }
+}
