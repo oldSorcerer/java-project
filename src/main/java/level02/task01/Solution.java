76aa2ab76ae75422e93541d91941a117656dae7f
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-03 20:18:13 +0300

    add solution level 02 task 01

---
+++ src/main/java/level02/task01/Solution.java
@@ -0,0 +1,11 @@
+package ru.javarush.java.core.level02.task01;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную heroAge типа int и сразу инициализируем её значением 25
+
+
+        // Выводим значение переменной heroAge на экран
+
+    }
+}
