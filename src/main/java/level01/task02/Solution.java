2a2fbd9054821f2acfd17c417dcdec89a25359fa
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-03 20:17:05 +0300

    add solution level 01 task 02

---
+++ src/main/java/level01/task02/Solution.java
@@ -0,0 +1,11 @@
+package ru.javarush.java.core.level01.task02;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Выводим текущий год 2025 на отдельной строке
+
+
+        // Выводим смайлик 😅 на отдельной строке
+
+    }
+}
