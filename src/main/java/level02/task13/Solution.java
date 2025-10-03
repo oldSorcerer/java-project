848b89cfa4ad0d8d1c6264ee365327b5cc77f498
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-03 20:19:28 +0300

    add solution level 02 task 13

---
+++ src/main/java/level02/task13/Solution.java
@@ -0,0 +1,12 @@
+package ru.javarush.java.core.level02.task13;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменные с информацией о персонаже
+
+
+        // Выводим все данные на экран
+
+
+    }
+}
