34c434dbe4e030d9442033e75ec5d7ca7725c5b2
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-03 20:19:37 +0300

    add solution level 02 task 15

---
+++ src/main/java/level02/task15/Solution.java
@@ -0,0 +1,12 @@
+package ru.javarush.java.core.level02.task15;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменные разных типов и присваиваем им произвольные значения
+
+
+        // Выводим информацию о размере каждого типа данных
+
+
+    }
+}
