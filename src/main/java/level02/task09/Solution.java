a0729902bdd731a068783cae2bf8b285cc730211
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-03 20:19:07 +0300

    add solution level 02 task 09

---
+++ src/main/java/level02/task09/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level02.task09;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную типа int и присваиваем ей произвольное целое значение (код доступа)
+
+
+        // Преобразуем целочисленное значение accessCode в строку
+
+
+        // Выводим полученную строку на экран
+
+    }
+}
