10b159002775bb22eb5512cd989715f7c683d5b4
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-03 20:18:46 +0300

    add solution level 02 task 05

---
+++ src/main/java/level02/task05/Solution.java
@@ -0,0 +1,11 @@
+package ru.javarush.java.core.level02.task05;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем строковую переменную и присваиваем ей сообщение из будущего
+
+
+        // Выводим сообщение на экран
+
+    }
+}
