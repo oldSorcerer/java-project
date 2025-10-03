bcaf2d12c9769781f7e769716e2a933cedc23f5f
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-03 20:20:08 +0300

    add solution level 02 task 20

---
+++ src/main/java/level02/task20/Solution.java
@@ -0,0 +1,19 @@
+package ru.javarush.java.core.level02.task20;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект Scanner для чтения данных с консоли
+
+
+        // Считываем имя пользователя (строка)
+
+
+        // Считываем возраст пользователя (целое число)
+
+
+        // Выводим отформатированную визитную карточку
+
+    }
+}
