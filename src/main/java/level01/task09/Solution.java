e4a75441910710430925ee706a3745b283459d0d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-03 20:17:42 +0300

    add solution level 01 task 09

---
+++ src/main/java/level01/task09/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level01.task09;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменные типа int и присваиваем им значения
+
+
+        // Вычисляем итоговую стоимость заказа
+
+
+        // Формируем итоговое сообщение
+
+
+        // Выводим итоговое сообщение на экран
+
+    }
+}
