1553d43cdadfb3d91af646bb47e926c105b33c4b
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-03 20:19:18 +0300

    add solution level 02 task 11

---
+++ src/main/java/level02/task11/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level02.task11;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем строковую переменную, содержащую год выпуска фильма
+
+
+        // Преобразуем строку в числовой тип int
+
+
+        // Выводим числовое значение года на экран
+
+    }
+}
