d6b0af4c39f4f405863c50f1d16f3ccb71601850
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-03 20:18:31 +0300

    add solution level 02 task 02

---
+++ src/main/java/level02/task02/Solution.java
@@ -0,0 +1,11 @@
+package ru.javarush.java.core.level02.task02;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем и инициализируем переменные в одной строке
+
+
+        // Выводим значение переменной totalScore на экран
+
+    }
+}
