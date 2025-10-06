26973829b2f62fe1dabfb8fb6454064546d2949c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-06 23:29:10 +0300

    add solution level 03 task 18

---
+++ src/main/java/level03/task18/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level03.task18;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную orderNumber и присваиваем ей значение
+
+
+        // Используем тернарный оператор для определения чётности orderNumber
+
+
+        // Выводим результат на экран
+
+    }
+}
