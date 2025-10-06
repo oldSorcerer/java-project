c9451d40d166a595b9a1a0620afe45ea5382580c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-06 23:43:12 +0300

    add solution level 05 task 07

---
+++ src/main/java/level05/task07/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level05.task07;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект Scanner для считывания данных с консоли
+
+
+        // Считываем большое число, введенное пользователем
+
+
+        // Выводим количество цифр в числе
+
+    }
+}
