21f0479c5de3e6f5328524a13b12d0f0d3096445
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-06 23:29:16 +0300

    add solution level 03 task 19

---
+++ src/main/java/level03/task19/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level03.task19;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную currentHour, которая хранит текущий час (от 0 до 23)
+
+
+        // Используем тернарный оператор для выбора приветствия
+
+
+        // Выводим приветственное сообщение на экран
+
+    }
+}
