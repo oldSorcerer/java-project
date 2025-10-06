bc1e06e6eaf65806f846892cc06bd79db31490ac
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-06 23:27:57 +0300

    add solution level 03 task 09

---
+++ src/main/java/level03/task09/Solution.java
@@ -0,0 +1,11 @@
+package ru.javarush.java.core.level03.task09;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную типа boolean с именем isJavaFun
+
+
+        // Выводим значение переменной isJavaFun на экран
+
+    }
+}
