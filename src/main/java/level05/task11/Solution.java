16df2e3ec4bf72351555f1530de85242fa6102a9
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-06 23:43:48 +0300

    add solution level 05 task 11

---
+++ src/main/java/level05/task11/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level05.task11;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int secret = rand.nextInt(100) + 1; // 1..100
+        Scanner console = new Scanner(System.in);
+
+        // Игра с духом
+        while (true) {
+
+        }
+    }
+}
