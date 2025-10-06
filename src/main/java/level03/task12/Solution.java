bf26c504751660f8dddccd9f7f926af62969bacd
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-06 23:28:18 +0300

    add solution level 03 task 12

---
+++ src/main/java/level03/task12/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level03.task12;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную roomTemperature типа int
+
+
+        // Проверяем, находится ли roomTemperature в диапазоне от 20 до 25 включительно
+
+
+        // Выводим значение переменной isComfortable на экран
+
+    }
+}
