7b390e73c0b439952b14a696b3ab1d02cb80b2ce
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-06 23:29:02 +0300

    add solution level 03 task 17

---
+++ src/main/java/level03/task17/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level03.task17;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем две переменные для времени спортсменов
+
+
+        // Используем тернарный оператор для определения минимального времени
+
+
+        // Выводим на экран наименьшее время
+
+    }
+}
