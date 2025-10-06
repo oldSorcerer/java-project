0a216476d0cd2fd6631db2a699cd503a5a8f65c5
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-06 23:28:26 +0300

    add solution level 03 task 13

---
+++ src/main/java/level03/task13/Solution.java
@@ -0,0 +1,15 @@
+package ru.javarush.java.core.level03.task13;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем исходные булевы переменные
+
+
+        // Важно: оператор && имеет более высокий приоритет, чем ||
+        // Поэтому сначала вычисляется (hasPlaneTickets && hasHotelRooms), затем результат объединяется с toTheSea через ||
+
+
+        // Выводим результат на экран
+
+    }
+}
