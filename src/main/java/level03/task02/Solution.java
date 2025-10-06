29880dd2e75a97faf19b742b986728857a080483
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-06 23:27:07 +0300

    add solution level 03 task 02

---
+++ src/main/java/level03/task02/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level03.task02;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную currentTemperature и присваиваем ей значение -5
+
+
+        // Проверяем, меньше ли температура 0
+        if (/* условие */) {
+            // Если температура меньше 0, выводим сообщение о морозе
+
+        } else {
+            // Если температура не меньше 0, выводим сообщение о легкой одежде
+
+        }
+    }
+}
