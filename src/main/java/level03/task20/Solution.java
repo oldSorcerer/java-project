f36679dd1f131733d562031c8c24ced26fef33ae
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-06 23:29:25 +0300

    add solution level 03 task 20

---
+++ src/main/java/level03/task20/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level03.task20;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную возраста посетителя
+
+
+        // Используем вложенные тернарные операторы для определения категории билета
+
+
+        // Выводим категорию билета на экран
+
+    }
+}
