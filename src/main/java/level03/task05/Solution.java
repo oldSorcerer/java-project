10db3414a4f4c4d1b68ebfe8f53998e89935e328
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-06 23:27:31 +0300

    add solution level 03 task 05

---
+++ src/main/java/level03/task05/Solution.java
@@ -0,0 +1,34 @@
+package ru.javarush.java.core.level03.task05;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект Scanner для ввода данных с клавиатуры
+        Scanner console = new Scanner(System.in);
+
+        // Запрашиваем у пользователя ввод возраста
+        System.out.println("Введите ваш возраст:");
+        int age = console.nextInt(); // Читаем возраст как целое число
+        console.nextLine(); // Поглощаем оставшийся символ новой строки
+
+        // Запрашиваем у пользователя ввод секретного кода
+        System.out.println("Введите секретный код:");
+        String secretCode = console.nextLine(); // Читаем секретный код как строку
+
+        // Проверяем возраст
+        if (/* условие */) {
+            // Если возраст >= 18, проверяем секретный код
+            if (/* условие */) {
+                // Если код равен "OPEN", разрешаем вход
+
+            } else {
+                // Если код не равен "OPEN", запрещаем вход
+
+            }
+        } else {
+            // Если возраст < 18, запрещаем вход
+
+        }
+    }
+}
