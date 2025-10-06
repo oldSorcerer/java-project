af308b8c850337cb12247d22be32840f7badc418
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-06 23:28:05 +0300

    add solution level 03 task 10

---
+++ src/main/java/level03/task10/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level03.task10;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем и присваиваем счёт нашей команды и соперника
+
+
+        // weWon — результат логического сравнения: true, если наш счёт больше
+
+
+        // Выводим значение переменной weWon на экран
+
+    }
+}
