baf5f27eda5ce85698da76e33e605e104ca974e1
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-06 23:42:42 +0300

    add solution level 05 task 03

---
+++ src/main/java/level05/task03/Solution.java
@@ -0,0 +1,18 @@
+package ru.javarush.java.core.level05.task03;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+
+
+        // Просим пользователя ввести строку (послание)
+
+
+        // Разворачиваем строку с помощью цикла for
+
+
+        // Выводим инвертированную строку
+
+    }
+}
