0e12efa2aac94a6564eba8ae3a24d5f1e94502cb
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-07 14:06:48 +0300

    add solution level 04 task 08

---
+++ src/main/java/level04/task08/Solution.java
@@ -0,0 +1,11 @@
+package ru.javarush.java.core.level04.task08;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Используем цикл for для одновременного изменения двух переменных
+        for (/* инициализация */; /* условие */; /* изменение */) {
+            // На каждой итерации выводим текущие значения x и y через пробел
+
+        }
+    }
+}
