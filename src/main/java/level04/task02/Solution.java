e2a9342cc9699b6b7962337a9b955eb818942b84
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-07 14:06:09 +0300

    add solution level 04 task 02

---
+++ src/main/java/level04/task02/Solution.java
@@ -0,0 +1,19 @@
+package ru.javarush.java.core.level04.task02;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        String input = ""; // будем хранить введённый пользователем текст
+
+        // Запрашиваем пароль, пока пользователь не введёт точное слово "java"
+        while (/* условие */) {
+
+        }
+
+        // Сообщение после успешного ввода
+
+    }
+}
