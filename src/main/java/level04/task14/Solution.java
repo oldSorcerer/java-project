99641769743baeb18690c73754ce36cac22a7de6
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-07 14:07:26 +0300

    add solution level 04 task 14

---
+++ src/main/java/level04/task14/Solution.java
@@ -0,0 +1,19 @@
+package ru.javarush.java.core.level04.task14;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Массив с оценками товаров
+        int[] productRatings = {3, -2, 7, -5, 8, 0};
+
+        // Цикл for для перебора всех элементов массива
+        for (int rating : productRatings) {
+            // Если товар бракованный (отрицательное число), пропускаем его
+            if (/*условие */) {
+                      // Переход к следующей итерации цикла
+            }
+
+            // Выводим на экран только качественные товары (положительные числа)
+            System.out.println(rating);
+        }
+    }
+}
