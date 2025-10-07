c653ad3aacb1da5e2fae0b1e3054ab32eab74427
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-07 14:06:01 +0300

    add solution level 04 task 01

---
+++ src/main/java/level04/task01/Solution.java
@@ -0,0 +1,19 @@
+package ru.javarush.java.core.level04.task01;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Инициализируем переменную для обратного отсчета
+
+
+        // Цикл while выполняется, пока значение countdown больше 0
+        while (/* условие */) {
+            // Выводим текущее значение countdown
+
+            // Уменьшаем значение countdown на 1
+
+        }
+
+        // После завершения цикла выводим сообщение "Старт!"
+
+    }
+}
