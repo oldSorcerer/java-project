a64e4fd9bbfce5c624c95b5a9b0a951534fd8fd3
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-07 14:07:02 +0300

    add solution level 04 task 10

---
+++ src/main/java/level04/task10/Solution.java
@@ -0,0 +1,22 @@
+package ru.javarush.java.core.level04.task10;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем Scanner для чтения данных с клавиатуры
+
+
+        // Используем do-while, чтобы запросить PIN хотя бы один раз
+        // и повторять ввод, пока число не будет в диапазоне 1000..9999
+        do {
+            // Запрос ввода PIN-кода
+
+            // читаем целое число
+
+        } while (/* условие */);
+
+        // Выводим сообщение о принятии PIN-кода
+
+    }
+}
