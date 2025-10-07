13a0f6ec34ca2c963dd11b3ff0dc70292ee46337
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-07 14:06:16 +0300

    add solution level 04 task 03

---
+++ src/main/java/level04/task03/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level04.task03;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Инициализируем переменную для номера кресла
+
+
+        // Используем цикл while для перебора номеров кресел
+        while (/* условие */) {
+            // Выводим текущий номер кресла на новой строке
+
+            // Увеличиваем номер кресла на 2
+
+        }
+    }
+}
