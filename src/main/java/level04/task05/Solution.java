f528126cc78509e269a976fcf1345aae48633b63
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-07 14:06:30 +0300

    add solution level 04 task 05

---
+++ src/main/java/level04/task05/Solution.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level04.task05;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Цикл for для обратного отсчёта от 5 до 1
+        for (/* инициализация */; /* условие */; /* изменение */) {
+            // Вывод текущего числа на новой строке
+
+        }
+        // Вывод поздравления после завершения цикла
+
+    }
+}
