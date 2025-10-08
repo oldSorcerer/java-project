578f464fd564e48707f3079ccd51521da960d007
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-08 23:48:05 +0300

    add solution level 06 task 10

---
+++ src/main/java/level06/task10/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level06.task10;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Делим 1.0 на 0.0: в мире double это не ошибка, а положительная бесконечность
+
+
+        // Выводим значение переменной (ожидается "Infinity")
+
+
+        // Подтверждаем, что значение действительно бесконечность
+
+    }
+}
