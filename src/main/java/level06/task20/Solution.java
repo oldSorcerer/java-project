c36ef4e1951819266ce66c16baff7ea12e4f1047
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-08 23:49:49 +0300

    add solution level 06 task 20

---
+++ src/main/java/level06/task20/Solution.java
@@ -0,0 +1,23 @@
+package ru.javarush.java.core.level06.task20;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную типа int для оценки за контрольную
+
+
+        // Объявляем переменную типа double для оценки за проект
+
+
+        // Вычисляем точный средний балл (double) и сохраняем в exactCourseAverage
+
+
+        // Преобразуем точный средний балл (double) в целое число (int) и сохраняем в roundedCourseAverage
+
+
+        // Выводим точный средний балл на экран
+
+
+        // Выводим округленный средний балл на экран
+
+    }
+}
