040ac156b3e42c7fab34a04c065fb156e9800101
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-08 23:46:22 +0300

    add solution level 06 task 03

---
+++ src/main/java/level06/task03/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level06.task03;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную типа char для хранения загадочного символа руны
+
+
+        // Преобразуем символ в его числовой код (Unicode) и сохраняем в переменную decryptedCode
+
+
+        // Выводим результат исследования: строку и расшифрованный числовой код символа
+
+    }
+}
