8431ab0e96c469f6636d4b524a8fd596ca0adef4
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-08 23:48:28 +0300

    add solution level 06 task 13

---
+++ src/main/java/level06/task13/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level06.task13;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Исходная оценка количества металла (в тоннах) с дробной частью
+
+
+        // Округляем до ближайшего целого числа с помощью Math.round (результат типа long)
+
+
+        // Выводим округленное значение на экран
+
+    }
+}
