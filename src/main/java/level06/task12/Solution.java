d8ec660328c3a568a72df7c2c9a0a98e3daa006b
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-08 23:48:20 +0300

    add solution level 06 task 12

---
+++ src/main/java/level06/task12/Solution.java
@@ -0,0 +1,20 @@
+package ru.javarush.java.core.level06.task12;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную actualFrequency и присваиваем ей значение 0.1 + 0.2
+
+
+        // Объявляем переменную targetFrequency и присваиваем ей значение 0.3
+
+
+        // Объявляем переменную tolerance и задаем ей значение 0.000001
+
+
+        // Вычисляем абсолютную разницу между actualFrequency и targetFrequency
+
+
+        // Сравниваем разницу с допуском и выводим соответствующее сообщение
+
+    }
+}
