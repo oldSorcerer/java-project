338b0972f793753b0de7f381aa5c1a6266a3021d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-08 23:47:19 +0300

    add solution level 06 task 09

---
+++ src/main/java/level06/task09/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level06.task09;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную mixtureResult и присваиваем ей результат сложения 0.1 + 0.2
+
+
+        // Объявляем переменную expectedConcentration и присваиваем ей значение 0.3
+
+
+        // Сравниваем значения переменных mixtureResult и expectedConcentration с помощью оператора ==
+
+
+        // Выводим результат сравнения на экран
+
+    }
+}
