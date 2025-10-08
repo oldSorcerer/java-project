a114e4a352665e4a2da614729710057621f23156
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-08 23:46:59 +0300

    add solution level 06 task 07

---
+++ src/main/java/level06/task07/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level06.task07;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную и присваиваем ей цену товара
+
+
+        // Форматируем число так, чтобы было ровно два знака после запятой
+
+
+        // Выводим отформатированное значение на экран
+
+    }
+}
