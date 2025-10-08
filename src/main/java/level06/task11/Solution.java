2ab6d7f2685ee934b9cc1baed6025e97805b1b2f
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-08 23:48:11 +0300

    add solution level 06 task 11

---
+++ src/main/java/level06/task11/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level06.task11;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Вычисляем квадратный корень из -1 с помощью Math.sqrt
+
+
+        // Выводим значение mysticAnomaly на экран
+
+
+        // Проверяем, является ли mysticAnomaly NaN (Not a Number)
+
+
+        // Выводим результат проверки на экран
+
+    }
+}
