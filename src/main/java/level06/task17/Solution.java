df0794fc39d7db30ad3f1c006990908619c12f49
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-08 23:49:00 +0300

    add solution level 06 task 17

---
+++ src/main/java/level06/task17/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level06.task17;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Общая масса материала в килограммах
+
+
+        // Явно приводим double к int — дробная часть отбрасывается
+
+
+        // Выводим количество целых предметов
+
+    }
+}
