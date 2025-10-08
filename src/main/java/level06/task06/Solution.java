bc25149812e5f9297482b5b79b3d06f35934f9d6
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-08 23:46:52 +0300

    add solution level 06 task 06

---
+++ src/main/java/level06/task06/Solution.java
@@ -0,0 +1,19 @@
+package ru.javarush.java.core.level06.task06;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект Scanner для считывания данных с клавиатуры
+
+
+        // Просим пользователя ввести сумму платежа
+
+
+        // Считываем введенное значение и сохраняем его в переменную типа double
+
+
+        // Подтверждаем полученную сумму, выводя её на экран
+
+    }
+}
