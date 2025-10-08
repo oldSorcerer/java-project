d9c66d73c7abf00d4de06ff47255a44be108e189
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-08 23:44:36 +0300

    add solution level 05 task 16

---
+++ src/main/java/level05/task16/Main.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level05.task16;
+
+// Импортируем класс java.util.Date
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        // Создаем объект java.util.Date и присваиваем ему текущую дату и время
+
+
+        // Создаем объект java.sql.Date, используя полное имя класса, и присваиваем ему текущую дату
+
+
+        // Выводим оба объекта на консоль
+
+    }
+}
