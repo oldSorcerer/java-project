bff9dbdea0142d6102fc0af2088e53da53c2775b
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-08 23:44:29 +0300

    add solution level 05 task 15

---
+++ src/main/java/level05/task15/Main.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level05.task15;
+
+public class Main {
+    public static void main(String[] args) {
+        // Создаем объект Scanner, используя полное имя класса
+
+
+        // Считываем строку, введенную пользователем
+
+
+        // Выводим считанную строку на экран
+        
+    }
+}
