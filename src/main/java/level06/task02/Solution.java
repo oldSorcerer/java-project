960e8011836fdc7885218326f28c3c493af4a33e
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-08 23:46:15 +0300

    add solution level 06 task 02

---
+++ src/main/java/level06/task02/Solution.java
@@ -0,0 +1,12 @@
+package ru.javarush.java.core.level06.task02;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную dragonHoard типа long и присваиваем ей 10 миллиардов
+        // Используем суффикс L и подчёркивания для удобства чтения
+
+        
+        // Выводим значение переменной на экран
+
+    }
+}
