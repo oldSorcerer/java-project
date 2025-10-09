708d9711c495d697f2178db1dd65ffc79af71561
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-10 00:03:32 +0300

    add solution level 09 task 17

---
+++ src/main/java/level09/task17/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level09.task17;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем StringBuilder с начальным содержимым "Java"
+        
+
+        // Добавляем к строке нужную фразу с помощью метода append()
+
+
+        // Выводим итоговую строку на экран
+
+    }
+}
