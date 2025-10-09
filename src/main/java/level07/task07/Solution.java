0cecd29b387de4e359d9ed719d2c1d2e5b5788e7
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-09 23:51:36 +0300

    add solution level 07 task 07

---
+++ src/main/java/level07/task07/Solution.java
@@ -0,0 +1,18 @@
+package ru.javarush.java.core.level07.task07;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем массив с быстрой инициализацией
+
+
+        // Переменная для хранения суммы
+
+
+        // Суммируем все элементы массива
+
+
+
+        // Выводим итоговую сумму на экран
+
+    }
+}
