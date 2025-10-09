1de94db3c6c70f8f4ab56f4cbe8c4f60b9dcc72b
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-10 00:04:39 +0300

    add solution level 10 task 03

---
+++ src/main/java/level10/task03/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level10.task03;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Исходная цена товара (примитивный тип double)
+        double productPrice = 3.14;
+
+        // Автоматическая упаковка (autoboxing) в объект типа-обёртки Double
+
+
+        // Автоматическая распаковка (unboxing) обратно в примитивный тип double
+
+
+        // Вывод финального "разупакованного" значения на экран
+
+    }
+}
