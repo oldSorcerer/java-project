c86a1b5817dce12398e682a5b38092ab9e0a048a
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-10 00:03:10 +0300

    add solution level 09 task 13

---
+++ src/main/java/level09/task13/Solution.java
@@ -0,0 +1,15 @@
+package ru.javarush.java.core.level09.task13;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем две строки в разном регистре
+        String s1 = "Hello";
+        String s2 = "HELLO";
+
+        // Сравниваем строки без учета регистра с помощью equalsIgnoreCase()
+
+
+        // Выводим результат сравнения (true или false)
+
+    }
+}
