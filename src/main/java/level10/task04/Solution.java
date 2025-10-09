e898bd48d31227a616e08fb401b5a0505fb10864
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-10 00:04:46 +0300

    add solution level 10 task 04

---
+++ src/main/java/level10/task04/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level10.task04;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Строковое значение, пришедшее от датчика
+        String sensorReadingText = "NaN";
+
+        // Преобразуем строку в число типа double с помощью класса Double
+
+
+        // Проверяем, является ли полученное значение "не числом" (NaN)
+
+
+        // Выводим результат проверки (true/false)
+
+    }
+}
