ef30c56603938aca32c45073421f7a3eee14f75c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-10 00:03:20 +0300

    add solution level 09 task 15

---
+++ src/main/java/level09/task15/Solution.java
@@ -0,0 +1,18 @@
+package ru.javarush.java.core.level09.task15;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем две строковые переменные с начальными значениями
+        String first = "apple";
+        String second = "banana";
+
+        // Сравниваем строки лексикографически:
+
+
+        // Меняем значения переменных местами
+
+
+        // Повторяем сравнение после обмена
+
+    }
+}
