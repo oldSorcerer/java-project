6aed83ab12c1b7fae8999ca1267739227d0aa87f
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-09 23:52:52 +0300

    add solution level 07 task 18

---
+++ src/main/java/level07/task18/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level07.task18;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем массив строк длиной 6 элементов
+
+
+        // Заполняем все элементы массива значением "Empty" с помощью метода Arrays.fill
+
+
+        // Выводим массив на экран в формате [Empty, Empty, Empty, Empty, Empty, Empty]
+
+    }
+}
