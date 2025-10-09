ce935fa94030398fb2e3a7324ec9c3e028d04885
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-10 00:04:58 +0300

    add solution level 10 task 06

---
+++ src/main/java/level10/task06/Solution.java
@@ -0,0 +1,11 @@
+package ru.javarush.java.core.level10.task06;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем константу: финальное целое число дней в календарной неделе
+
+
+        // Выводим значение константы на экран
+
+    }
+}
