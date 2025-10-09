9b5ae6b9b04481130751cfeb61d5bc6c7d89f8ae
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-10 00:02:59 +0300

    add solution level 09 task 11

---
+++ src/main/java/level09/task11/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level09.task11;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Исходная строка
+        String word = "programming";
+
+        // Извлечение подстроки "gram" с помощью метода substring()
+
+
+        // Вывод подстроки на экран
+
+    }
+}
