c372cdb396183be2f0c13ecc206f48451db7f841
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-09 23:52:59 +0300

    add solution level 07 task 19

---
+++ src/main/java/level07/task19/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level07.task19;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Исходный массив температур за день
+
+
+        // Копируем элементы с индексами 2 (включительно) до 5 (не включая 5): 30, 40, 50
+
+
+        // Выводим полученный подмассив в формате [30, 40, 50]
+        
+    }
+}
