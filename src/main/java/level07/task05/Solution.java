956f32ca3bb7100acd5693263d309b8b9ab1c2d7
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-09 23:51:23 +0300

    add solution level 07 task 05

---
+++ src/main/java/level07/task05/Solution.java
@@ -0,0 +1,12 @@
+package ru.javarush.java.core.level07.task05;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Быстрая инициализация массива из 5 элементов
+
+
+        // Выводим каждый элемент на новой строке
+
+
+    }
+}
