9f48e4203e9518065a7c78dc4416884a2336432d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-09 23:51:29 +0300

    add solution level 07 task 06

---
+++ src/main/java/level07/task06/Solution.java
@@ -0,0 +1,21 @@
+package ru.javarush.java.core.level07.task06;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем массив фиксированной длины 3 для любимых фильмов
+
+
+        // Создаем Scanner для чтения строк с клавиатуры
+
+
+        // Считываем три названия фильмов и сохраняем по порядку в массив
+
+
+
+        // Выводим фильмы в обратном порядке: третий, второй, первый
+
+
+    }
+}
