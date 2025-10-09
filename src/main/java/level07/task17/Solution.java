fa26baef61d4d819aa2083d087866046183bd57d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-09 23:52:47 +0300

    add solution level 07 task 17

---
+++ src/main/java/level07/task17/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level07.task17;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем массив с заданными значениями
+
+
+        // Сортируем массив по возрастанию с помощью метода Arrays.sort
+
+
+        // Выводим отсортированный массив на экран с использованием Arrays.toString
+
+    }
+}
