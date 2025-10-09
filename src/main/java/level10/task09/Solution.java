def3dff654d12e3861da1792819813557a42e870
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-10 00:05:15 +0300

    add solution level 10 task 09

---
+++ src/main/java/level10/task09/Solution.java
@@ -0,0 +1,15 @@
+package ru.javarush.java.core.level10.task09;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем Scanner для чтения числа с клавиатуры
+        Scanner console = new Scanner(System.in);
+        int day = console.nextInt();
+
+        // Используем классический switch для определения дня недели
+
+        
+    }
+}
