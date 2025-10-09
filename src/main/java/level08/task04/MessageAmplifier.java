0918e2f1df0e65efcee0152f16f4b604ea75bfe0
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-09 23:53:56 +0300

    add solution level 08 task 04

---
+++ src/main/java/level08/task04/MessageAmplifier.java
@@ -0,0 +1,18 @@
+package ru.javarush.java.core.level08.task04;
+
+public class MessageAmplifier {
+
+    // Публичный статический метод, который повторяет строку и выводит результат
+    public static void amplifyAndDisplay(String messageText, int numberOfRepeats) {
+        // Используем обычную строку и оператор +
+
+
+        // Выводим итоговую "усиленную" строку
+
+    }
+
+    public static void main(String[] args) {
+        // Демонстрация работы: усиливаем сообщение "Ура!" четыре раза
+        amplifyAndDisplay("Ура!", 4);
+    }
+}
