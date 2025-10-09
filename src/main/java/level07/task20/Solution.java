4dfe2487586998b13a3dedb4d737d8a70546ba27
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-09 23:53:07 +0300

    add solution level 07 task 20

---
+++ src/main/java/level07/task20/Solution.java
@@ -0,0 +1,18 @@
+package ru.javarush.java.core.level07.task20;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем основной код доступа
+
+        // Создаем резервный код доступа
+
+
+        // Сравниваем содержимое двух массивов поэлементно с помощью Arrays.equals
+
+
+        // Выводим результат сравнения (true, если массивы идентичны, иначе false)
+
+    }
+}
