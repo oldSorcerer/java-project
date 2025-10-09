2db949865e71de8a30dc81809f8aa2dd0a487ee7
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-10 00:05:08 +0300

    add solution level 10 task 08

---
+++ src/main/java/level10/task08/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level10.task08;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем константу с модификатором final и присваиваем ей значение 100
+
+
+        // Попытка изменить значение константы
+
+
+        // Выводим значение константы на экран
+
+    }
+}
