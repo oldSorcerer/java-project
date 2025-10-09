9bdfc39d5c7c7b66c46dba27e41974d9a4a33612
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-10 00:02:39 +0300

    add solution level 09 task 09

---
+++ src/main/java/level09/task09/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level09.task09;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем строку с секретным словом
+        String secret = "Java";
+
+        // Извлекаем символ с индексом 1 (второй символ, т.к. индексация с нуля) методом charAt()
+
+
+        // Выводим извлеченный символ на экран
+
+    }
+}
