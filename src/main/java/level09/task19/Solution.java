61d5422303810019a2d765dd6160246e07df2483
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-10 00:03:44 +0300

    add solution level 09 task 19

---
+++ src/main/java/level09/task19/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level09.task19;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем StringBuilder с исходной строкой
+
+        
+        // Разворачиваем строку с помощью стандартного метода reverse()
+
+
+        // Выводим результат на экран: должно получиться "fedcba"
+
+    }
+}
