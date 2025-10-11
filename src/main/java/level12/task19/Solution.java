235b05a289166e6116038ff50639ed3eff4c1966
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-12 00:08:31 +0300

    add solution level 12 task 19

---
+++ src/main/java/level12/task19/Solution.java
@@ -0,0 +1,22 @@
+package ru.javarush.java.core.level12.task19;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        // Создаем HTTP‑клиент по умолчанию
+
+
+        // Готовим GET‑запрос к API фактов о кошках
+
+
+        // Отправляем запрос и получаем ответ в виде строки
+
+
+        // Выводим тело ответа на экран (JSON-строка с фактом о кошках)
+
+    }
+}
