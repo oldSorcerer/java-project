edebd6bc0eb2968412d56535e6664a5601e0cc25
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-11 23:59:49 +0300

    add solution level 11 task 13

---
+++ src/main/java/level11/task13/Solution.java
@@ -0,0 +1,19 @@
+package ru.javarush.java.core.level11.task13;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Solution {
+
+    // Метод объявляет проброс исключения: обработка ошибки перекладывается на вызывающий код
+
+        // Пытаемся открыть файл по указанному пути.
+        // Если файл не найден, конструктор FileReader выбросит FileNotFoundException.
+
+
+
+    // Небольшая демонстрация использования
+    public static void main(String[] args) throws FileNotFoundException {
+
+    }
+}
