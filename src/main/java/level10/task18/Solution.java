2ada6bff1fbb946b6f7b3cabde60db1fae0b61a9
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-11 23:57:51 +0300

    add solution level 10 task 18

---
+++ src/main/java/level10/task18/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level10.task18;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную droneCommand и присваиваем ей значение "pause"
+        String droneCommand = "pause";
+
+        // Используем современное switch-выражение для определения статуса дрона
+
+
+        // Выводим сообщение о статусе дрона на экран
+
+    }
+}
