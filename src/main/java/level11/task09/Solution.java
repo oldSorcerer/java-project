e3c9f5f2c418c285060abf7386a1656ba30b6056
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-11 23:59:22 +0300

    add solution level 11 task 09

---
+++ src/main/java/level11/task09/Solution.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level11.task09;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        // Выполняем деление 10 на 2 и выводим результат
+
+
+        // Сообщение о завершении работы робота, которое будет выведено всегда
+
+
+    }
+}
