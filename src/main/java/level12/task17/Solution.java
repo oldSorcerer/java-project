096638aad71f81344c32ba598e24ca4855a1143a
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-12 00:08:18 +0300

    add solution level 12 task 17

---
+++ src/main/java/level12/task17/Solution.java
@@ -0,0 +1,22 @@
+package ru.javarush.java.core.level12.task17;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        // Создаем HTTP-клиент (современный API из java.net.http)
+
+
+        // Формируем GET-запрос к заданному адресу
+
+
+        // Отправляем запрос и получаем ответ как строку (полный JSON)
+
+
+        // Выводим полный JSON-ответ на экран
+
+    }
+}
