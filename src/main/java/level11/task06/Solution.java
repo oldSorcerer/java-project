bd907e1a42f4ee1bbe4284c54c977c4bbd5fbe1f
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-11 23:59:01 +0300

    add solution level 11 task 06

---
+++ src/main/java/level11/task06/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level11.task06;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Рюкзак героя на 3 слота: храним ID предметов
+
+
+        try {
+            // Ошибочный доступ к 10-му индексу (за пределами массива из 3 элементов)
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            // Сообщение для игрока о выходе индекса за границы массива
+
+
+        }
+    }
+}
