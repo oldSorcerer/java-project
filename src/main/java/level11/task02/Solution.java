d9e9e9ba5dbed5c5d0bda1c75d746072f0e6aac7
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-11 23:58:37 +0300

    add solution level 11 task 02

---
+++ src/main/java/level11/task02/Solution.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level11.task02;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем массив collectedGems длиной 3
+
+
+        // Пытаемся вывести элемент по индексу 5.
+        // В массиве всего 3 элемента (индексы 0..2), поэтому ниже возникнет исключение
+        // ArrayIndexOutOfBoundsException.
+
+    }
+}
