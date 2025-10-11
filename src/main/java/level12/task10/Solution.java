fbae76a3a5bb7337674de0be7a26a6e64651d5bf
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-12 00:07:36 +0300

    add solution level 12 task 10

---
+++ src/main/java/level12/task10/Solution.java
@@ -0,0 +1,18 @@
+package ru.javarush.java.core.level12.task10;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        // Задаем путь к файлу "note.txt" с помощью класса Path
+
+
+        // Считываем весь текст файла целиком удобным методом Files.readString
+
+
+        // Выводим считанное содержимое без добавления лишней новой строки
+
+    }
+}
