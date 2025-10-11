a0c37f21da75965f3a9edda87161aa714e3df687
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-12 00:00:02 +0300

    add solution level 11 task 15

---
+++ src/main/java/level11/task15/Solution.java
@@ -0,0 +1,30 @@
+package ru.javarush.java.core.level11.task15;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Solution {
+
+    public static void extractFirstLineFromData(String dataFile)  {
+        // Создаем FileReader для чтения файла
+
+        
+        // Оборачиваем FileReader в BufferedReader для построчного чтения
+
+        
+        // Читаем первую строку из файла
+
+        
+        // Закрываем BufferedReader (он автоматически закроет и FileReader)
+
+        
+        // Выводим первую строку на экран (опционально, если требуется)
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
