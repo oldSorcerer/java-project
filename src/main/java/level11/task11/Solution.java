4af8573529d09f0540315b2038d4b8be05bc211d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-11 23:59:37 +0300

    add solution level 11 task 11

---
+++ src/main/java/level11/task11/Solution.java
@@ -0,0 +1,11 @@
+package ru.javarush.java.core.level11.task11;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Намеренно вызываем арифметическую ошибку, деля 5 на 0
+
+
+        // Блок finally выполняется в любом случае, даже если произошло исключение
+
+    }
+}
