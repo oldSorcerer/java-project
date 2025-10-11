15f3db87375a75727e30bff651437cd979d64fc3
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-12 00:07:04 +0300

    add solution level 12 task 05

---
+++ src/main/java/level12/task05/Solution.java
@@ -0,0 +1,19 @@
+package ru.javarush.java.core.level12.task05;
+
+import java.util.HashMap;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем ассоциативный массив (HashMap) для хранения переводов слов
+
+
+        // Добавляем в словарь перевод: ключ "дом", значение "house"
+
+
+        // Получаем перевод слова "дом" из словаря
+
+
+        // Выводим перевод на экран
+
+    }
+}
