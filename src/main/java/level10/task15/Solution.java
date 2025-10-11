b52d161dfb0b14e214dd0d46a55120a244151c31
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-11 23:57:31 +0300

    add solution level 10 task 15

---
+++ src/main/java/level10/task15/Solution.java
@@ -0,0 +1,11 @@
+package ru.javarush.java.core.level10.task15;
+
+// Перечисление дней недели: каждое значение — отдельный день.
+
+
+public class Solution {
+    public static void main(String[] args) {
+        // Используем цикл for-each для перебора всех значений перечисления DayOfWeek
+
+    }
+}
