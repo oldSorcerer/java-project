b2ec3f864d05e38b79f1c85b3e3ba9463f3084ff
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-12 00:07:43 +0300

    add solution level 12 task 11

---
+++ src/main/java/level12/task11/Solution.java
@@ -0,0 +1,21 @@
+package ru.javarush.java.core.level12.task11;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        // 1) Создаем массив байтов с ASCII-кодами букв H, I, J
+
+
+        // 2) Записываем массив в бинарный файл "letters.bin"
+
+
+        // 3) Читаем содержимое файла обратно в новый массив байтов
+
+
+        // 4) Выводим каждый байт как символ, разделяя пробелами
+
+    }
+}
