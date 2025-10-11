0137c2cc2b087d8f911c44fa5d2ac7197522d6f7
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-12 00:00:09 +0300

    add solution level 11 task 16

---
+++ src/main/java/level11/task16/Solution.java
@@ -0,0 +1,18 @@
+package ru.javarush.java.core.level11.task16;
+
+public class Solution {
+
+    // Метод для преобразования строки в число с пробросом исключения NumberFormatException
+    public static int convertToGoldAmount(String playerInput)  {
+        // Используем Integer.parseInt для преобразования строки в число
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        // Пробуем преобразовать некорректную строку в число
+
+
+        // Обрабатываем исключение и выводим сообщение об ошибке
+
+    }
+}
