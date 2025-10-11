d3702e4eb3ff1259488f040bacf8ad058ec76b02
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-12 00:06:52 +0300

    add solution level 12 task 03

---
+++ src/main/java/level12/task03/Solution.java
@@ -0,0 +1,19 @@
+package ru.javarush.java.core.level12.task03;
+
+import java.util.ArrayList;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем динамический список для хранения имен гостей
+
+
+        // Добавляем имена в список
+
+
+        // Проверяем, содержится ли имя "Петр" в списке
+
+
+        // Проверяем, содержится ли имя "Сергей" в списке
+
+    }
+}
