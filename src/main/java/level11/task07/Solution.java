c55a4ea939cc1a9340865015e3402f37eacc84a8
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-11 23:59:07 +0300

    add solution level 11 task 07

---
+++ src/main/java/level11/task07/Solution.java
@@ -0,0 +1,15 @@
+package ru.javarush.java.core.level11.task07;
+
+public class Solution {
+    public static void main(String[] args) {
+        try {
+            // Пробуем поделить 50 на 0 — это вызовет ArithmeticException
+
+
+        } catch (ArithmeticException e) {
+            // Выводим текстовое описание ошибки, сгенерированное JVM
+
+
+        }
+    }
+}
