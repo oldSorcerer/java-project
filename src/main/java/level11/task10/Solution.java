7e5a376e2e27a89fb324e52caf8cf0c2a8b7d9fe
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-11 23:59:31 +0300

    add solution level 11 task 10

---
+++ src/main/java/level11/task10/Solution.java
@@ -0,0 +1,22 @@
+package ru.javarush.java.core.level11.task10;
+
+public class Solution {
+    // Метод выводит положительный счёт или генерирует исключение для неположительных значений
+    public static void displayPositiveScore(int currentScore) {
+
+        // Генерируем IllegalArgumentException с требуемым сообщением
+
+
+    }
+
+    public static void main(String[] args) {
+        // Корректный вызов: положительное значение
+
+
+        // Некорректный вызов: отрицательное значение, перехватываем исключение
+
+        // Выводим сообщение перехваченного исключения
+
+
+    }
+}
