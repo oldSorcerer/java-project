a080864c54f71223dd7e8a65ce6a0cdf20848b62
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-11 23:59:43 +0300

    add solution level 11 task 12

---
+++ src/main/java/level11/task12/Solution.java
@@ -0,0 +1,22 @@
+package ru.javarush.java.core.level11.task12;
+
+public class Solution {
+
+    // Метод для проверки имени пользователя
+    public static void validateUserName(String userName) {
+        // Проверяем, является ли имя пользователя пустым
+
+        // Генерируем исключение IllegalArgumentException с заданным сообщением
+
+
+        // Сообщение, которое выводится всегда, независимо от результата проверки
+
+    }
+
+    public static void main(String[] args) {
+        // Вызываем метод validateUserName с пустой строкой
+
+        // Перехватываем исключение и выводим его сообщение
+
+    }
+}
