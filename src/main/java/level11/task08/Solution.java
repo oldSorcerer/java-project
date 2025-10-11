4baf1c0f97f32e2226c11e4605f1867ff3e79512
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-11 23:59:13 +0300

    add solution level 11 task 08

---
+++ src/main/java/level11/task08/Solution.java
@@ -0,0 +1,21 @@
+package ru.javarush.java.core.level11.task08;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменные primaryValue и secondaryValue
+
+
+        try {
+            // Пытаемся выполнить деление primaryValue на secondaryValue
+
+
+        } catch (ArithmeticException e) {
+            // Обрабатываем исключение деления на ноль
+
+
+        }
+
+        // Выводим финальное сообщение, подтверждающее завершение программы
+
+    }
+}
