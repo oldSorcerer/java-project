797d7935d88cfb26559fcca8a18fc692a929fba9
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-11 23:58:05 +0300

    add solution level 10 task 20

---
+++ src/main/java/level10/task20/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level10.task20;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем балл студента
+        int studentScore = 9;
+
+        // Новое switch-выражение: возвращает значение, а не просто выполняет блок кода.
+
+
+        // Выводим итоговую оценку на экран
+
+    }
+}
