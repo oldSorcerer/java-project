32d647d9ab622ae49c8072b064b38f4c4e90b63a
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-12 00:06:38 +0300

    add solution level 12 task 01

---
+++ src/main/java/level12/task01/Solution.java
@@ -0,0 +1,19 @@
+package ru.javarush.java.core.level12.task01;
+
+import java.util.ArrayList;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем динамический список для хранения числовых идентификаторов артефактов
+
+
+        // Добавляем идентификатор 42 в список
+
+
+        // Получаем первый элемент списка по индексу 0
+
+
+        // Выводим значение первого элемента на экран
+
+    }
+}
