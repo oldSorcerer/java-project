279a4a282b4102be4959bbd6f3fa208ac2f736c9
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-11 23:58:55 +0300

    add solution level 11 task 05

---
+++ src/main/java/level11/task05/Solution.java
@@ -0,0 +1,21 @@
+package ru.javarush.java.core.level11.task05;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменные по условию
+
+
+        try {
+            // Пытаемся выполнить деление, которое вызовет ArithmeticException (деление на ноль)
+
+
+            // Этот вывод здесь для наглядности (не будет выполнен при stepsPerUnit = 0)
+
+
+        } catch (ArithmeticException e) {
+            // Дружелюбное сообщение для оператора при делении на ноль
+
+
+        }
+    }
+}
