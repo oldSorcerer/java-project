39827b8bf14050dd5171bbb207c5b6b28c4994fe
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-11 23:57:23 +0300

    add solution level 10 task 14

---
+++ src/main/java/level10/task14/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level10.task14;
+
+// Перечисление с четырьмя временами года
+
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем переменную типа Season и присваиваем ей значение SUMMER
+
+
+        // Для каждого сезона выводим свое жизнерадостное сообщение
+
+    }
+}
