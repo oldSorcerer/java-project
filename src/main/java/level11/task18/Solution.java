bfd981a6c3eaf25c003c15b46af7ef78e6f7d4c3
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-12 00:06:08 +0300

    add solution level 11 task 18

---
+++ src/main/java/level11/task18/Solution.java
@@ -0,0 +1,12 @@
+package ru.javarush.java.core.level11.task18;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем массив playerInventory размером 3 элемента
+
+
+        // Пытаемся вывести элемент массива с индексом 5
+        // Это вызовет исключение ArrayIndexOutOfBoundsException
+
+    }
+}
