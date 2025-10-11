de5698722960c1697ec54baafa09dffce93c13b4
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-11 23:58:28 +0300

    add solution level 11 task 01

---
+++ src/main/java/level11/task01/Solution.java
@@ -0,0 +1,15 @@
+package ru.javarush.java.core.level11.task01;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменные согласно условию
+
+
+        // Попытка выполнить деление на ноль.
+        // В момент вычисления этой строки произойдет ArithmeticException: / by zero
+
+
+        // Попытка вывести результат деления (до этой строки выполнение не дойдет из-за исключения выше)
+
+    }
+}
