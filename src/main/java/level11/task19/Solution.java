2fdc8b94a39e0fb87a4d7841f2b8932345707eac
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-12 00:06:15 +0300

    add solution level 11 task 19

---
+++ src/main/java/level11/task19/Solution.java
@@ -0,0 +1,10 @@
+package ru.javarush.java.core.level11.task19;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        // Вызов метода calculateReportData из main
+
+    }
+
+}
