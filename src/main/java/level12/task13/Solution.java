4e27abc4a3728937f01965037e7254e4c48b32e0
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-12 00:07:53 +0300

    add solution level 12 task 13

---
+++ src/main/java/level12/task13/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level12.task13;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        // URL изображения, которое нужно скачать
+        String imageUrl = "https://httpbin.org/image/png";
+        // Имя файла, в который будет сохранено изображение
+        String fileName = "image01.png";
+
+
+    }
+}
