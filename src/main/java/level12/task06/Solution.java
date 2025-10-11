224d6e78c55ac121f6eff5a1a3a095ef0f39104a
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-12 00:07:10 +0300

    add solution level 12 task 06

---
+++ src/main/java/level12/task06/Solution.java
@@ -0,0 +1,18 @@
+package ru.javarush.java.core.level12.task06;
+
+import java.util.HashMap;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем ассоциативный массив: ключ — имя студента, значение — его оценка
+
+
+        // Добавляем запись: "Анна" -> 5
+
+
+        // Проверяем, есть ли в словаре оценка для "Анны"
+
+            // Получаем оценку и выводим сообщение
+
+    }
+}
