3d8f57fe17ab966d8528af832ebe25438a8aa2e2
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-11 23:57:15 +0300

    add solution level 10 task 13

---
+++ src/main/java/level10/task13/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level10.task13;
+
+// Перечисление состояний светофора.
+// Содержит ровно три значения: RED, YELLOW и GREEN.
+
+
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаём переменную типа TrafficSignalState и задаём начальное значение RED
+
+
+        // Выводим текущее состояние светофора на экран
+
+    }
+}
