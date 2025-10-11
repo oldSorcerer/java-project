2be3787216fdd5fec95cc7e2f4b901be966572fd
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-11 23:57:43 +0300

    add solution level 10 task 17

---
+++ src/main/java/level10/task17/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level10.task17;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Числовой код HTTP-статуса
+        int httpStatusCode = 200;
+
+        // Новый синтаксис switch-выражения:
+
+
+        // Вывод результата на экран
+
+    }
+}
