8971054c4a5afdda2411dc3ed60c827eee02cd86
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:53:00 +0300

    add solution level 13 task 18

---
+++ src/main/java/level13/task18/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level13.task18;
+
+import java.time.LocalDate;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Текущая дата
+
+
+        // Контрольная дата: 1 января 2025 года
+
+
+        // Используем isBefore: если futureMilestone раньше today, значит 2025 уже наступил
+
+    }
+}
