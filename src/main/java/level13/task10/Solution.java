2305f6db293185fee86a71ff589998e8c583650f
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:52:15 +0300

    add solution level 13 task 10

---
+++ src/main/java/level13/task10/Solution.java
@@ -0,0 +1,22 @@
+package ru.javarush.java.core.level13.task10;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class Solution {
+    public static void main(String[] args) {
+        // 1. Создаем "локальное" время без привязки к часовой зоне:
+        // 1 июня 2025 года, 14:00
+
+
+        // 2. Выбираем часовую зону Europe/Minsk
+
+
+        // 3. Привязываем локальное время к выбранной зоне, получаем ZonedDateTime
+
+
+        // 4. Выводим результат для проверки (например: 2025-06-01T14:00+03:00[Europe/Minsk])
+
+    }
+}
