ad5d5dd4992769d5225139779944f9b6b4cab651
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:51:06 +0300

    add solution level 13 task 01

---
+++ src/main/java/level13/task01/Solution.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level13.task01;
+
+import java.time.LocalDate;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем переменную типа LocalDate и записываем в нее сегодняшнюю дату
+        
+
+        // Выводим дату на экран
+
+    }
+}
