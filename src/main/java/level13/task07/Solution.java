a9605c9f5296f80c0bc22325bcd43ee5bc40fc63
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:51:58 +0300

    add solution level 13 task 07

---
+++ src/main/java/level13/task07/Solution.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level13.task07;
+
+import java.time.LocalTime;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем два объекта времени: утренняя встреча и дневная презентация
+
+
+        // Сравниваем времена с помощью специализированного метода isBefore
+
+    }
+}
