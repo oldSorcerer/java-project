0a9b0aa081e0f9908e0f648390b1ef2459e0b2a5
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:54:00 +0300

    add solution level 14 task 07

---
+++ src/main/java/level14/task07/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level14.task07;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем и "запускаем" счётчик
+
+
+        // "Нажимаем кнопку" три раза
+
+
+        // Просим счётчик показать текущее значение (ожидается число 3)
+
+    }
+}
