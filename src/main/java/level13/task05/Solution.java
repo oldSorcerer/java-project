e29470a4b4ac5981a3bb9daf10c5af6181233824
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:51:48 +0300

    add solution level 13 task 05

---
+++ src/main/java/level13/task05/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level13.task05;
+
+import java.time.LocalTime;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Получаем текущее время с помощью LocalTime.now()
+
+
+        // Извлекаем часы, минуты и секунды из текущего времени
+
+
+        // Выводим часы, минуты и секунды в заданном формате
+
+    }
+}
