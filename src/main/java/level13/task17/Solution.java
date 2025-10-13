6c54b0cfa81ef2aa99be3fb94cd8b7f4676a88a0
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:52:56 +0300

    add solution level 13 task 17

---
+++ src/main/java/level13/task17/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level13.task17;
+
+import java.time.LocalDate;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Получаем сегодняшнюю дату
+
+
+        // Прибавляем к ней 10 дней, чтобы узнать дату начала обратного отсчета
+
+
+        // Выводим результат на экран
+
+    }
+}
