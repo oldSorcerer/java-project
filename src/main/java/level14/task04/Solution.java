dd7e301f52584be3ee55c3a3c3b78652a611abf1
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:53:41 +0300

    add solution level 14 task 04

---
+++ src/main/java/level14/task04/Solution.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level14.task04;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем два объекта Student и сразу инициализируем их поля через конструктор
+
+
+        // Выводим информацию о каждом студенте в заданном формате
+
+    }
+}
+
+// Класс-форма для студента: хранит имя и год поступления
