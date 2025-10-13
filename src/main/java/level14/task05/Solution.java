77bbe9c63992096ad11fa4b3625e10e99d690325
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:53:47 +0300

    add solution level 14 task 05

---
+++ src/main/java/level14/task05/Solution.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level14.task05;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект Car с произвольными значениями марки и года выпуска
+
+
+        // Просим автомобиль "представиться"
+
+    }
+}
+
+// Класс Car хранит марку и год выпуска и умеет выводить информацию о себе
