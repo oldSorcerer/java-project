2ad91ea02c637377c082352a2d3fd3224c0ee892
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:55:18 +0300

    add solution level 14 task 19

---
+++ src/main/java/level14/task19/Solution.java
@@ -0,0 +1,10 @@
+package ru.javarush.java.core.level14.task19;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Запускаем "сборку" новой машины: при создании объекта сработает нестатический блок инициализации
+        
+    }
+}
+
+// "Чертёж" автомобиля
