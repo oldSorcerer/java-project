aeec66d01020fde1b5e3d6017d9a876c10e7a007
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:53:06 +0300

    add solution level 13 task 19

---
+++ src/main/java/level13/task19/Solution.java
@@ -0,0 +1,20 @@
+package ru.javarush.java.core.level13.task19;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Текущая дата (берется из системных настроек)
+        
+
+        // Дата дедлайна проекта: 15 мая 2025 года
+
+
+        // Количество дней между today и projectDeadline
+
+
+        // Выводим результат на экран
+
+    }
+}
