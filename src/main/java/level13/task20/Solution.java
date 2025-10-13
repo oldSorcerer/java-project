13efbc2b0109544d770a4b87160d69cce9d1bc20
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:53:13 +0300

    add solution level 13 task 20

---
+++ src/main/java/level13/task20/Solution.java
@@ -0,0 +1,20 @@
+package ru.javarush.java.core.level13.task20;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект LocalDate для даты постройки здания: 20 марта 2000 года
+
+        
+        // Получаем текущую дату
+
+        
+        // Вычисляем полный период между датой постройки и сегодняшним днем
+
+        
+        // Форматируем и выводим результат в формате "Возраст: X лет, Y месяцев, Z дней"
+
+    }
+}
