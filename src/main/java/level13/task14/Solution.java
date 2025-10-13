448399b5a6b7bb935596688e3b07a22b324a70b3
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:52:40 +0300

    add solution level 13 task 14

---
+++ src/main/java/level13/task14/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level13.task14;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Входная строка с датой в формате ISO (год-месяц-день)
+        String inputDateString = "2024-12-31";
+
+        // Парсим строку в объект LocalDate, явно указывая стандартный ISO форматтер
+
+
+        // Выводим полученный объект даты на экран
+
+    }
+}
