ea36a3eba75aca02fff8340fb3fe936996910793
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:52:26 +0300

    add solution level 13 task 12

---
+++ src/main/java/level13/task12/Solution.java
@@ -0,0 +1,21 @@
+package ru.javarush.java.core.level13.task12;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class Solution {
+    public static void main(String[] args) {
+        // 1) Создаем ZonedDateTime для 1 июня 2025 года, 12:00 в зоне Europe/Minsk
+
+
+        // 2) Преобразуем ZonedDateTime в абсолютное мгновение Instant (UTC, часовой пояс не влияет)
+
+
+        // 3) Тот же самый Instant отображаем как локальное время для зоны Asia/Tokyo
+
+
+        // 4) Выводим все три значения
+
+    }
+}
