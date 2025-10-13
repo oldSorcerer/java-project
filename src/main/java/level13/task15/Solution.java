bcec9a73737eb53f6ab97a6e1cd359c8cb7d4373
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:52:46 +0300

    add solution level 13 task 15

---
+++ src/main/java/level13/task15/Solution.java
@@ -0,0 +1,20 @@
+package ru.javarush.java.core.level13.task15;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект LocalDate с датой 15 марта 2023 года
+
+
+        // Готовим форматтер с шаблоном "dd.MM.yyyy" (например, "15.03.2023")
+
+
+        // Форматируем дату в строку с учетом заданного шаблона
+
+
+        // Выводим получившуюся строку на экран
+
+    }
+}
