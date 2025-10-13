117ffc80a41f39f75011fb441cd03e7cf4e26f0c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:51:37 +0300

    add solution level 13 task 03

---
+++ src/main/java/level13/task03/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level13.task03;
+
+import java.time.LocalDate;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Получаем текущую дату и сохраняем в переменной today
+
+
+        // Вычисляем завтрашнюю дату на основе today
+
+
+        // Выводим завтрашнюю дату на экран
+
+    }
+}
