54219885245177505370ec1304bbc139b606b83b
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:51:43 +0300

    add solution level 13 task 04

---
+++ src/main/java/level13/task04/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level13.task04;
+
+import java.time.LocalDateTime;
+
+public class Solution {
+    public static void main(String[] args) {
+        // LocalDateTime хранит одновременно дату и время
+        // Фиксируем точный момент: 20 марта 2022 года, 10:00
+
+
+        // Выводим зафиксированный момент на экран
+
+    }
+}
