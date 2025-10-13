bd94a041f2239ea48b077b1f75d98f3a2247ac3e
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:51:53 +0300

    add solution level 13 task 06

---
+++ src/main/java/level13/task06/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level13.task06;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект LocalDate для 15 декабря 1990 года
+
+
+        // Получаем день недели для указанной даты
+
+
+        // Выводим день недели на экран
+
+    }
+}
