5588f98b1b6bddbcba4b171caf268eeec4d75b7a
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:52:51 +0300

    add solution level 13 task 16

---
+++ src/main/java/level13/task16/Solution.java
@@ -0,0 +1,20 @@
+package ru.javarush.java.core.level13.task16;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Исходная строка с меткой времени заказа
+
+
+        // Создаем форматтер с шаблоном "dd.MM.yyyy HH:mm"
+
+
+        // Парсим строку в объект LocalDateTime
+
+
+        // Выводим результат в стандартном ISO-формате (например, 2025-06-01T14:30)
+
+    }
+}
