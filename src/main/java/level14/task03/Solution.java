1454a7d12e5947d9b5727e5f63a0039eac64e1a2
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:53:34 +0300

    add solution level 14 task 03

---
+++ src/main/java/level14/task03/Solution.java
@@ -0,0 +1,11 @@
+package ru.javarush.java.core.level14.task03;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем одну конкретную книгу
+
+
+        // Просим книгу "рассказать о себе"
+
+    }
+}
