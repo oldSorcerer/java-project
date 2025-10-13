a2b83ef3a1f18b09583afa04bd426fdebf8e32a3
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:53:29 +0300

    add solution level 14 task 02

---
+++ src/main/java/level14/task02/Solution.java
@@ -0,0 +1,11 @@
+package ru.javarush.java.core.level14.task02;
+
+// Основной класс с точкой входа в программу
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем и инициализируем переменную myCat,
+        
+    }
+}
+
+// Шаблон (чертёж) для создания объектов-котов
