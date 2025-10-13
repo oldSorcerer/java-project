9a05a6a7bf9e9208ddb948628de8f4b5fde7ea81
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:52:09 +0300

    add solution level 13 task 09

---
+++ src/main/java/level13/task09/Solution.java
@@ -0,0 +1,18 @@
+package ru.javarush.java.core.level13.task09;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Получаем текущее время в каждой указанной зоне через ZonedDateTime.now(ZoneId.of(...))
+
+
+        // Простой и наглядный формат даты и времени (локальное время конкретной зоны)
+
+
+        // Выводим каждую зону на отдельной строке: название города + локальное время
+
+    }
+}
