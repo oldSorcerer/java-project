ea39d6180fa7098f942c4d0c9ed51c9d20274452
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:52:34 +0300

    add solution level 13 task 13

---
+++ src/main/java/level13/task13/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level13.task13;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Получаем сегодняшнюю дату как LocalDate
+        
+
+        // Форматируем дату в стандарт ISO_LOCAL_DATE (например, 2025-06-01)
+
+
+        // Выводим отформатированную дату
+
+    }
+}
