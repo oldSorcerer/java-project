c1c8865a7524c00b87c147352f211a3fc089669a
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-13 19:52:04 +0300

    add solution level 13 task 08

---
+++ src/main/java/level13/task08/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level13.task08;
+
+import java.time.LocalDateTime;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Исходная дата и время брифинга: 1 июня 2025 года, 14:00
+
+
+        // Прибавляем 2 часа, затем вычитаем 30 минут и сохраняем результат.
+
+
+        // Выводим итоговое время старта миссии
+
+    }
+}
