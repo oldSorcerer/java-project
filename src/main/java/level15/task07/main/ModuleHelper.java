5a6a83db14beb87105d9646f45a1a7d9f834815f
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-14 19:56:53 +0300

    add solution level 15 task 07

---
+++ src/main/java/level15/task07/main/ModuleHelper.java
@@ -0,0 +1,7 @@
+package ru.javarush.java.core.level15.task07.main;
+
+public class ModuleHelper {
+    // Метод без модификатора доступа — package-private.
+    // Доступен только внутри этого же пакета.
+
+}
---
+++ src/main/java/level15/task07/main/ModuleMain.java
@@ -0,0 +1,8 @@
+package ru.javarush.java.core.level15.task07.main;
+
+public class ModuleMain {
+    public static void main(String[] args) {
+        // Классы в одном пакете имеют доступ к package-private методам
+
+    }
+}
