ae2b3db34ddd56e11f5020bf87202c1667b36a0f
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-14 19:59:40 +0300

    add solution level 16 task 03

---
+++ src/main/java/level16/task03/Solution.java
@@ -0,0 +1,15 @@
+package ru.javarush.java.core.level16.task03;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект библиотеки
+//        Library library = new Library();
+
+        // Создаем объект нестатического внутреннего класса через объект внешнего класса
+        // Синтаксис library.new Scroll(...) обязателен для non-static inner классов
+//        Library.Scroll scroll = library.new Scroll("Ancient Runes");
+
+        // Показываем название свитка
+//        scroll.revealTitle();
+    }
+}
