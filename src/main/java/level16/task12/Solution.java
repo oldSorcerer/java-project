a1fd297af2d41cd34b3865a696f84f584f10d43d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-15 20:00:58 +0300

    add solution level 16 task 12

---
+++ src/main/java/level16/task12/Solution.java
@@ -0,0 +1,25 @@
+package ru.javarush.java.core.level16.task12;
+
+// Главный класс с методом main
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект лаборатории и запускаем механизм раскрытия секрета
+        SecureLab lab = new SecureLab();
+        lab.revealSecret();
+    }
+}
+
+// Класс секретной лаборатории
+class SecureLab {
+    // Приватное поле с секретом
+    private String classifiedSecret = "Секрет раскрыт!";
+
+    // Метод, который "раскрывает" секрет через анонимный класс
+    void revealSecret() {
+        // Создаем объект интерфейса Runnable через анонимный класс
+
+
+        // Вызываем метод run() прямо здесь, без создания отдельного потока
+        revealMechanism.run();
+    }
+}
