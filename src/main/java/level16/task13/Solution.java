cb61465413f68bf1eeda5b6464d1cb1c4e566f07
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-15 20:01:04 +0300

    add solution level 16 task 13

---
+++ src/main/java/level16/task13/Solution.java
@@ -0,0 +1,21 @@
+package ru.javarush.java.core.level16.task13;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Небольшая демонстрация работы доски объявлений
+        DisplayBoard board = new DisplayBoard();
+        board.showTemporaryMessage();
+    }
+}
+
+class DisplayBoard {
+
+    // Метод, внутри которого объявляется локальный класс
+    public void showTemporaryMessage() {
+        // Локальный класс доступен только в пределах этого метода
+
+
+        // Создаем объект локального класса и вызываем его метод
+
+    }
+}
