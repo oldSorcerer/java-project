29de075d1dbd4fea65657e069c12761ca7fe6aca
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-15 20:00:46 +0300

    add solution level 16 task 10

---
+++ src/main/java/level16/task10/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level16.task10;
+
+// Объявляем интерфейс Communicator с единственным методом greet
+interface Communicator {
+    void greet();
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект анонимного класса, реализующего Communicator
+
+
+        // Вызываем метод greet(), чтобы вывести приветствие на экран
+
+    }
+}
