24b5418172502befa5c730e47f1ec693b772c33c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-15 20:00:40 +0300

    add solution level 16 task 09

---
+++ src/main/java/level16/task09/Solution.java
@@ -0,0 +1,19 @@
+package ru.javarush.java.core.level16.task09;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект MagicalCreature с помощью анонимного класса
+
+
+        // Вызываем метод, чтобы услышать уникальный звук
+
+    }
+}
+
+// Базовый класс с общим поведением
+class MagicalCreature {
+    void makeSound() {
+        // Общий звук для любых магических существ
+        System.out.println("Существо издаёт звук");
+    }
+}
