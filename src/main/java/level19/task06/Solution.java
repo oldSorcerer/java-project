8e8259e9bf591cf8deb91b1c6ac19ea98b7a2af1
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-17 21:34:43 +0300

    add solution level 19 task 06

---
+++ src/main/java/level19/task06/Solution.java
@@ -0,0 +1,14 @@
+package level19.task06;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем "умное" устройство — телефон
+        Phone phone = new Phone();
+
+        // Включаем телефон (способ включения индивидуален для каждого устройства)
+        phone.turnOn();
+
+        // Выключаем устройство (общий алгоритм для всех устройств)
+        phone.turnOff();
+    }
+}
