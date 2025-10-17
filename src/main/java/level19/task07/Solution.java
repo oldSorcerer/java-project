b2bfb9f9edaeb12d8e95f9c1c3b2179328f466cc
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-17 21:34:48 +0300

    add solution level 19 task 07

---
+++ src/main/java/level19/task07/Solution.java
@@ -0,0 +1,15 @@
+package level19.task07;
+
+// Главный класс с точкой входа
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем круг с именем "Круг" и радиусом 2.0
+        Circle circle = new Circle("Круг", 2.0);
+
+        // Сначала выводим имя фигуры
+        System.out.println(circle.name);
+
+        // Затем выводим площадь круга
+        System.out.println(circle.area());
+    }
+}
