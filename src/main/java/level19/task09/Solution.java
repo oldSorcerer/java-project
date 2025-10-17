46d4189da1a83c791fe8e250119a49dc6c45608d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-17 21:34:59 +0300

    add solution level 19 task 09

---
+++ src/main/java/level19/task09/Solution.java
@@ -0,0 +1,11 @@
+package level19.task09;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Полиморфизм: переменная типа Vehicle ссылается на объект Car
+        Vehicle car = new Car();
+
+        // Вызов переопределённого метода move() у машины
+        car.move(); // Ожидаемый вывод: "Машина едет"
+    }
+}
