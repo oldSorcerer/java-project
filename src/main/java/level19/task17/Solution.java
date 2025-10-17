11d5d404f541c79981879561e37616c9513fdb58
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-17 21:35:57 +0300

    add solution level 19 task 17

---
+++ src/main/java/level19/task17/Solution.java
@@ -0,0 +1,9 @@
+package level19.task17;
+
+// Небольшая демонстрация работы
+public class Solution {
+    public static void main(String[] args) {
+        Task task = new SimpleTask("Купить кофе");
+        task.complete(); // Ожидаемый вывод: Задача 'Купить кофе' выполнена
+    }
+}
