3269f94f333b4cecde59d3a87b7787e40dce6678
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-17 21:34:31 +0300

    add solution level 19 task 04

---
+++ src/main/java/level19/task04/Solution.java
@@ -0,0 +1,10 @@
+package level19.task04;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем платеж PayPal и последовательно вызываем общую и специфичную логику
+        PaypalPayment payment = new PaypalPayment();
+        payment.printInfo();   // сначала общая информация
+        payment.process();     // затем специфическая обработка
+    }
+}
