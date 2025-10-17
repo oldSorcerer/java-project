96c51cf3d1c190839d55f4b012ace5ca7dcf0f28
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-17 21:35:23 +0300

    add solution level 19 task 13

---
+++ src/main/java/level19/task13/Solution.java
@@ -0,0 +1,11 @@
+package level19.task13;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаём круг радиуса 2.0
+        Circle circle = new Circle(2.0);
+
+        // Выводим площадь. Ожидаем увидеть 12.56
+        System.out.println(circle.area());
+    }
+}
