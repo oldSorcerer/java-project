f1d88c977d03901d620f3ebc2010d71594f12e80
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-17 21:34:16 +0300

    add solution level 19 task 02

---
+++ src/main/java/level19/task02/Solution.java
@@ -0,0 +1,9 @@
+package level19.task02;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Демонстрация работы: полиморфный вызов makeSound()
+        Animal dog = new Dog();
+        dog.makeSound(); // Ожидаемый вывод: Гав!
+    }
+}
