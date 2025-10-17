0251df442b122e6cdc54d449d965caa55d74a236
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-17 21:35:17 +0300

    add solution level 19 task 12

---
+++ src/main/java/level19/task12/Solution.java
@@ -0,0 +1,16 @@
+package level19.task12;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем массив базового типа Payment: можно хранить любые его наследники
+        Payment[] payments = new Payment[] {
+                new CashPayment(),    // экземпляр оплаты наличными
+                new OnlinePayment()   // экземпляр онлайн-оплаты
+        };
+
+        // Полиморфный вызов: для каждого объекта будет вызвана своя реализация process()
+        for (Payment payment : payments) {
+            payment.process();
+        }
+    }
+}
