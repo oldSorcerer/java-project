2f0b24ef2a8a13dddb5fd8cb3ab1d4b0435cac8e
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-17 21:35:42 +0300

    add solution level 19 task 15

---
+++ src/main/java/level19/task15/Solution.java
@@ -0,0 +1,20 @@
+package level19.task15;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Динамический список, способный хранить объекты типа Vehicle (полиморфизм)
+        List<Vehicle> vehicles = new ArrayList<>();
+
+        // Добавляем разные виды транспорта
+        vehicles.add(new Car("Lada"));
+        vehicles.add(new Bicycle("Stels"));
+
+        // Проходим по списку и вызываем move() у каждого транспорта
+        for (Vehicle v : vehicles) {
+            v.move();
+        }
+    }
+}
