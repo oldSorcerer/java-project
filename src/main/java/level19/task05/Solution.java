190cb06e140c942f8cfb222ce57e699335a0f9ed
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-17 21:34:38 +0300

    add solution level 19 task 05

---
+++ src/main/java/level19/task05/Solution.java
@@ -0,0 +1,9 @@
+package level19.task05;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем собаку и просим её издать звук
+        Animal dog = new Dog();
+        dog.makeSound(); // Ожидается вывод: Гав!
+    }
+}
