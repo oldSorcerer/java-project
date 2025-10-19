6e1bc5eaabc03912ce42ae7cf25c950fda6e1429
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-19 17:32:40 +0300

    add solution level 01 task 04

--- src/main/java/level01/task04/Solution.java
+++ src/main/java/level01/task04/Solution.java
@@ -3,9 +3,9 @@ package ru.javarush.java.core.level01.task04;
 public class Solution {
     public static void main(String[] args) {
         // Объявляем переменную типа int с именем currentYear
-
+        int currentYear = 2025;
 
         // Выводим значение переменной currentYear на экран
-
+        System.out.println(currentYear);
     }
 }
