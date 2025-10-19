aef818f26f9bdf21f59b0df2664a74d759054abf
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-19 17:33:00 +0300

    add solution level 01 task 07

--- src/main/java/level01/task07/Solution.java
+++ src/main/java/level01/task07/Solution.java
@@ -3,12 +3,13 @@ package ru.javarush.java.core.level01.task07;
 public class Solution {
     public static void main(String[] args) {
         // Объявляем переменные для хранения очков двух игроков и сразу инициализируем их нужными значениями
-
+        int player1Score = 1999;
+        int player2Score = 25;
 
         // Вычисляем общую сумму очков
-
+        int totalScore = player1Score + player2Score;
 
         // Выводим на экран значение общей суммы очков
-
+        System.out.println(totalScore);
     }
 }
