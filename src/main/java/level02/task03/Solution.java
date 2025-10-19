cabedc72d2bd1d323fba93e52aa126d38c513261
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-19 17:34:05 +0300

    add solution level 02 task 03

--- src/main/java/level02/task03/Solution.java
+++ src/main/java/level02/task03/Solution.java
@@ -3,17 +3,19 @@ package ru.javarush.java.core.level02.task03;
 public class Solution {
     public static void main(String[] args) {
         // Объявляем общее количество призов и число команд
-
+        int prizes = 15;
+        int teams = 4;
 
         // Вычисляем, сколько целых призов получит каждая команда (целочисленное деление)
-
+        int prizesPerTeam = prizes / teams;
 
         // Вычисляем, сколько призов останется после раздачи (оператор %)
-
+        int remainder = prizes % teams;
 
         // Выводим количество призов на команду
+        System.out.println(prizesPerTeam);
 
         // Выводим остаток призов
-
+        System.out.println(remainder);
     }
 }
