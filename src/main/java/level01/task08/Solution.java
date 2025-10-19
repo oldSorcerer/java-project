35229c18d52ee433d4c80e451321af2f5867421d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-19 17:33:07 +0300

    add solution level 01 task 08

--- src/main/java/level01/task08/Solution.java
+++ src/main/java/level01/task08/Solution.java
@@ -3,15 +3,15 @@ package ru.javarush.java.core.level01.task08;
 public class Solution {
     public static void main(String[] args) {
         // Объявляем переменную типа String для имени пользователя
-
+        String userName = "Alice";
 
         // Объявляем переменную типа int для возраста пользователя
-
+        int userAge = 20;
 
         // Создаем строку, объединяющую имя и возраст пользователя
-
+        String profileInfo = String.format("Имя: %s, Возраст: %d", userName, userAge);
 
         // Выводим информацию о профиле на экран
-
+        System.out.println(profileInfo);
     }
 }
