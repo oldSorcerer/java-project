df797188e47d956da450d0351a50301135f9f95a
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-19 17:33:20 +0300

    add solution level 01 task 10

--- src/main/java/level01/task10/Solution.java
+++ src/main/java/level01/task10/Solution.java
@@ -3,15 +3,15 @@ package ru.javarush.java.core.level01.task10;
 public class Solution {
     public static void main(String[] args) {
         // Объявляем переменную типа int и присваиваем ей значение 10
-        int number = 10;
+//        int number = 10;
 
         // Объявляем переменную типа String и присваиваем ей значение "Hello, World!"
-        String message = "Hello, World!";
+//        String message = "Hello, World!";
 
         // Выводим значение переменной number на экран
-        System.out.println(number);
+//        System.out.println(number);
 
         // Выводим значение переменной message на экран
-        System.out.println(message);
+//        System.out.println(message);
     }
 }
