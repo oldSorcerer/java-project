a9772f8da075c8ece114a6d398dcc9ac3712e8c9
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-19 17:34:22 +0300

    add solution level 02 task 05

--- src/main/java/level02/task05/Solution.java
+++ src/main/java/level02/task05/Solution.java
@@ -3,9 +3,10 @@ package ru.javarush.java.core.level02.task05;
 public class Solution {
     public static void main(String[] args) {
         // Объявляем строковую переменную и присваиваем ей сообщение из будущего
-
+        String futureMessage = "Good news everyone!";
 
         // Выводим сообщение на экран
+        System.out.println(futureMessage);
 
     }
 }
