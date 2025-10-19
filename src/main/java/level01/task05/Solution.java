a2d1c88a5453bd9456d1d536cd35e886079ce473
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-19 17:32:47 +0300

    add solution level 01 task 05

--- src/main/java/level01/task05/Solution.java
+++ src/main/java/level01/task05/Solution.java
@@ -3,9 +3,9 @@ package ru.javarush.java.core.level01.task05;
 public class Solution {
     public static void main(String[] args) {
         // Объявляем переменную типа String с именем myMessage
-
+        String myMessage = "I ❤\uFE0F YOU";
 
         // Выводим значение переменной myMessage на экран
-
+        System.out.println(myMessage);
     }
 }
