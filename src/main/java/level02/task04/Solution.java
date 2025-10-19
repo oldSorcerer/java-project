b6e4bb6dbc9fee47ff584e771f8012c5d86637de
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-19 17:34:14 +0300

    add solution level 02 task 04

--- src/main/java/level02/task04/Solution.java
+++ src/main/java/level02/task04/Solution.java
@@ -3,12 +3,12 @@ package ru.javarush.java.core.level02.task04;
 public class Solution {
     public static void main(String[] args) {
         // Объявляем и инициализируем уровень жизни героя
-
+        int heroHealth = 10;
 
         // Увеличиваем значение на 1 (инкремент) и выводим результат
-
+        System.out.println(++heroHealth);
 
         // Уменьшаем значение на 1 (декремент) и снова выводим результат
-
+        System.out.println(--heroHealth);
     }
 }
