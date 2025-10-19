aee282cffcdc4ee328ae9a937d20c832408947ef
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-19 17:34:56 +0300

    add solution level 02 task 08

--- src/main/java/level02/task08/Solution.java
+++ src/main/java/level02/task08/Solution.java
@@ -3,19 +3,20 @@ package ru.javarush.java.core.level02.task08;
 public class Solution {
     public static void main(String[] args) {
         // Объявляем строковую переменную с лишними пробелами в начале и конце
-
+        String cityName = "  Minsk  ";
 
         // Выводим длину строки до удаления пробелов
-
+        System.out.println(cityName.length());
 
         // Удаляем пробелы по краям и сохраняем результат обратно в переменную
-
+        cityName = cityName.trim();
 
         // Выводим длину строки после удаления пробелов
-
+        System.out.println(cityName.length());
 
         // Выводим название города в верхнем и нижнем регистре
-
+        System.out.println(cityName.toUpperCase());
+        System.out.println(cityName.toLowerCase());
 
     }
 }
