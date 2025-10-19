41dcc797133162e4c596c1baa59249fe0b62abb4
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-19 17:32:20 +0300

    add solution level 01 task 01

--- src/main/java/level01/task01/Solution.java
+++ src/main/java/level01/task01/Solution.java
@@ -3,6 +3,6 @@ package ru.javarush.java.core.level01.task01;
 public class Solution {
     public static void main(String[] args) {
         // Выводим на экран фразу "Круто быть программистом!😎"
-        
+        System.out.println("Круто быть программистом!\uD83D\uDE0E");
     }
 }
