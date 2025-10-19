cc69e7d1fa3bffcffa402eb37a41a7cdc869ad98
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-19 17:35:07 +0300

    add solution level 02 task 09

--- src/main/java/level02/task09/Solution.java
+++ src/main/java/level02/task09/Solution.java
@@ -3,12 +3,12 @@ package ru.javarush.java.core.level02.task09;
 public class Solution {
     public static void main(String[] args) {
         // Объявляем переменную типа int и присваиваем ей произвольное целое значение (код доступа)
-
+        int accessCode = 15;
 
         // Преобразуем целочисленное значение accessCode в строку
-
+        String string = String.valueOf(accessCode);
 
         // Выводим полученную строку на экран
-
+        System.out.println(string);
     }
 }
