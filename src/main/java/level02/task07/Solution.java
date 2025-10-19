ae7c4318e4e8d1badd605539cc6b968903612d5d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-19 17:34:48 +0300

    add solution level 02 task 07

--- src/main/java/level02/task07/Solution.java
+++ src/main/java/level02/task07/Solution.java
@@ -3,9 +3,9 @@ package ru.javarush.java.core.level02.task07;
 public class Solution {
     public static void main(String[] args) {
         // Внутренние двойные кавычки экранируем обратным слэшем
-
+        String heroQuote = "\"Поехали!\"";
 
         // Выводим значение переменной на экран
-
+        System.out.println(heroQuote);
     }
 }
