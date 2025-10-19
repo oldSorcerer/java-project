aac7fdf0f3a83471e811a058280d82763b51c4c3
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-19 17:34:30 +0300

    add solution level 02 task 06

--- src/main/java/level02/task06/Solution.java
+++ src/main/java/level02/task06/Solution.java
@@ -3,12 +3,13 @@ package ru.javarush.java.core.level02.task06;
 public class Solution {
     public static void main(String[] args) {
         // Объявляем переменные и присваиваем им значения
-
+        String firstName = "Luke";
+        String lastName = "Skywalker";
 
         // Объединяем имя и фамилию через пробел
-
+        String fullName = firstName + " " + lastName;
 
         // Выводим получившееся полное имя
-
+        System.out.println(fullName);
     }
 }
