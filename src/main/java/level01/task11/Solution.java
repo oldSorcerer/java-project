531aee8b8c8351734e382c9191ce0b502e57dc15
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-19 17:33:27 +0300

    add solution level 01 task 11

--- src/main/java/level01/task11/Solution.java
+++ src/main/java/level01/task11/Solution.java
@@ -2,7 +2,7 @@ package ru.javarush.java.core.level01.task11;
 
 public class Solution {
     public static void main(String[] args) {
-
+/*
         // Пример объявления переменной типа int
         int number = 10;
 
@@ -12,6 +12,6 @@ public class Solution {
         // Пример вывода переменных на экран
         System.out.println(number);
         System.out.println(text);
-
+*/
     }
 }
