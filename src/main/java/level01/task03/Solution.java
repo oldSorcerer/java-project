ed24f52cc92d5c4c2816d080099d2706315760c1
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-19 17:32:33 +0300

    add solution level 01 task 03

--- src/main/java/level01/task03/Solution.java
+++ src/main/java/level01/task03/Solution.java
@@ -3,15 +3,19 @@ package ru.javarush.java.core.level01.task03;
 public class Solution {
     public static void main(String[] args) {
         // Выводим первое слово первой строки
+        System.out.print("Я ");
 
         // Добавляем пробел и выводим второе слово первой строки
+        System.out.print("учу ");
 
         // Добавляем пробел и выводим третье слово первой строки
+        System.out.println("Java.");
 
-        
         // Выводим первое слово второй строки
+        System.out.print("Это ");
 
         // Добавляем пробел и выводим второе слово второй строки
+        System.out.println("интересно!");
 
     }
 }
