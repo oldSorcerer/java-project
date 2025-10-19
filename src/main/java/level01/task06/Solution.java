5505b3f06b5fadde140eb68ce2b36b66e3782cbb
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-19 17:32:54 +0300

    add solution level 01 task 06

--- src/main/java/level01/task06/Solution.java
+++ src/main/java/level01/task06/Solution.java
@@ -3,15 +3,15 @@ package ru.javarush.java.core.level01.task06;
 public class Solution {
     public static void main(String[] args) {
         // Объявляем переменную currentYear и присваиваем ей значение 2025
-
+        int currentYear = 2025;
 
         // Объявляем переменную projectAge
-
+        int projectAge;
 
         // Вычисляем возраст проекта как разницу между currentYear и 2019
-
+        projectAge = currentYear - 2019;
 
         // Выводим возраст проекта на экран
-
+        System.out.println(projectAge);
     }
 }
