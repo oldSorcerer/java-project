77d79d3f03d273d005cf7effe9ad4347ba35e299
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-19 17:33:13 +0300

    add solution level 01 task 09

--- src/main/java/level01/task09/Solution.java
+++ src/main/java/level01/task09/Solution.java
@@ -3,15 +3,17 @@ package ru.javarush.java.core.level01.task09;
 public class Solution {
     public static void main(String[] args) {
         // Объявляем переменные типа int и присваиваем им значения
-
+        int itemPrice = 4;
+        int itemCount = 3;
+        int discount = 2;
 
         // Вычисляем итоговую стоимость заказа
-
+        int totalCost = (itemPrice + itemCount) * discount;
 
         // Формируем итоговое сообщение
-
+        String finalMessage = "Итоговая стоимость: " + totalCost;
 
         // Выводим итоговое сообщение на экран
-
+        System.out.println(finalMessage);
     }
 }
