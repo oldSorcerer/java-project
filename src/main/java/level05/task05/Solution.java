cc56719c11d18e33a3c264b137ff4f2e1d69f466
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-21 17:53:54 +0300

    add solution level 05 task 05

--- src/main/java/level05/task05/Solution.java
+++ src/main/java/level05/task05/Solution.java
@@ -5,26 +5,28 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         // Создаем сканер для чтения числа с консоли
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите высоту обелиска (целое число): ");
+        int pyramidHeight = scanner.nextInt();
 
         // Строим обелиск строка за строкой
-        for (/* инициализация */; /* условие */; /* изменение */) {
+        for (int i = 0; i < pyramidHeight; i++) {
             // Количество пробелов слева: уменьшается от (N - 1) до 0
-
+            int spaces = pyramidHeight - i - 1;
 
             // Количество звездочек: растет по формуле (2 * row - 1)
-
+            int stars = (2 * (i + 1)) - 1;
 
             // Печатаем пробелы для выравнивания
-            for (/* инициализация */; /* условие */; /* изменение */) {
-
+            for (int j = 0; j < spaces; j++) {
+                System.out.print(" ");
             }
             // Печатаем звездочки
-            for (/* инициализация */; /* условие */; /* изменение */) {
-
+            for (int j = 0; j < stars; j++) {
+                System.out.print("*");
             }
             // Переход на новую строку после каждой строки обелиска
-
+            System.out.println();
         }
     }
 }
