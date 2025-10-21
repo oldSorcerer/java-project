ec774c98122466316bfbfe7f8879c311f3a01445
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-21 17:41:53 +0300

    add solution level 08 task 04

--- src/main/java/level08/task04/MessageAmplifier.java
+++ src/main/java/level08/task04/MessageAmplifier.java
@@ -5,10 +5,10 @@ public class MessageAmplifier {
     // Публичный статический метод, который повторяет строку и выводит результат
     public static void amplifyAndDisplay(String messageText, int numberOfRepeats) {
         // Используем обычную строку и оператор +
-
+        String repeat = messageText.repeat(numberOfRepeats);
 
         // Выводим итоговую "усиленную" строку
-
+        System.out.println(repeat);
     }
 
     public static void main(String[] args) {
