aae8e2eea712ca763bce5cb4a3f3c570d24f18ce
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-21 17:53:48 +0300

    add solution level 05 task 04

--- src/main/java/level05/task04/Solution.java
+++ src/main/java/level05/task04/Solution.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         // Создаем объект Scanner для ввода данных с клавиатуры
-
+        Scanner scanner = new Scanner(System.in);
 
         // Запрашиваем у пользователя ввод числа N (magicLimit)
-
+        int magicLimit = scanner.nextInt();
 
         // Вычисляем и выводим квадрат каждого числа от 1 до N
-        for (/* инициализация */; /* условие */; /* изменение */) {
-            // Вывод квадрата числа
+        for (int i = 1; i <= magicLimit; i++) {
+            System.out.println(i * i);
 
         }
     }
