cf0e0f2bd9e70f1e5f2f8f69dbe9a23f6f5cf816
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-21 17:41:39 +0300

    add solution level 07 task 02

--- src/main/java/level07/task02/Solution.java
+++ src/main/java/level07/task02/Solution.java
@@ -1,14 +1,16 @@
 package ru.javarush.java.core.level07.task02;
 
+import java.util.Arrays;
+
 public class Solution {
     public static void main(String[] args) {
         // Создаем массив строк с фиксированным размером 3
-
+        String[] favoriteLanguages = new String[3];
 
         // Заполняем массив значениями "Java"
-
+        Arrays.fill(favoriteLanguages, "Java");
 
         // Выводим на экран количество элементов, которые способен хранить массив
-
+        System.out.println(favoriteLanguages.length);
     }
 }
