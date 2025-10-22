cc629b3c8e158fa8467a7b6cb3008909cd039ac1
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-22 17:59:05 +0300

    add solution level 14 task 06

--- src/main/java/level14/task06/Solution.java
+++ src/main/java/level14/task06/Solution.java
@@ -3,11 +3,20 @@ package ru.javarush.java.core.level14.task06;
 public class Solution {
     public static void main(String[] args) {
         // "Зачисляем" нового студента, сразу задавая имя и оценку в конструкторе
-
+        Student student = new Student("Ivan", 5);
 
         // Торжественно объявляем о его появлении
-
+        System.out.printf("Имя: %s, оценка: %d%n", student.studentName, student.studentGrade);
     }
 }
 
 // Отдельный класс Student с полями и конструктором
+class Student {
+    String studentName;
+    int studentGrade;
+
+    public Student(String studentName, int studentGrade) {
+        this.studentName = studentName;
+        this.studentGrade = studentGrade;
+    }
+}
