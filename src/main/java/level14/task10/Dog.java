fbc208ffa92a93d47e54cf3219b082711d604f0c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-22 17:59:28 +0300

    add solution level 14 task 10

--- src/main/java/level14/task10/Dog.java
+++ src/main/java/level14/task10/Dog.java
@@ -2,16 +2,20 @@ package ru.javarush.java.core.level14.task10;
 
 public class Dog {
     // Поля с основной информацией о собаке
-
+    String dogName;
+    int dogAge;
 
     // Конструктор "регистрации": сразу присваиваем имя и возраст
-
+    public Dog(String dogName, int dogAge) {
+        this.dogName = dogName;
+        this.dogAge = dogAge;
+    }
 
     public static void main(String[] args) {
         // "Поступление" новой собаки в приют: задаём имя и возраст через конструктор
-
+        Dog dog = new Dog("Шарик", 5);
 
         // Торжественное объявление о прибытии
-
+        System.out.printf( "Собаку зовут %s, ей %d лет%n", dog.dogName, dog.dogAge);
     }
 }
