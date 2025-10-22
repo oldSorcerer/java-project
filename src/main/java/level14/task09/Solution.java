0b6f801a97d35fbc9d866e60425233104926a1af
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-22 17:59:22 +0300

    add solution level 14 task 09

--- src/main/java/level14/task09/Solution.java
+++ src/main/java/level14/task09/Solution.java
@@ -3,11 +3,21 @@ package ru.javarush.java.core.level14.task09;
 public class Solution {
     public static void main(String[] args) {
         // Создаем нового кота с именем "Барсик" и возрастом 3 года (используем "new" и конструктор)
-
+        Cat cat = new Cat("Барсик", 3);
 
         // Выводим имя и возраст кота, чтобы подтвердить корректность создания объекта
-
+        System.out.println(cat.catName);
+        System.out.println(cat.catAge);
     }
 }
 
 // Шаблон (класс) кота с полями для имени и возраста
+class Cat {
+    String catName;
+    int catAge;
+
+    public Cat(String catName, int catAge) {
+        this.catName = catName;
+        this.catAge = catAge;
+    }
+}
