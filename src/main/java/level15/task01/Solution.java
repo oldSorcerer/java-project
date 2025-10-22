85a08d64bb122fe7533b9fa4366d852731376c96
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-22 17:59:48 +0300

    add solution level 15 task 01

--- src/main/java/level15/task01/Solution.java
+++ src/main/java/level15/task01/Solution.java
@@ -3,7 +3,7 @@ package ru.javarush.java.core.level15.task01;
 public class Solution {
     public static void main(String[] args) {
         // Создаем экземпляр класса Person
-//        Person person = new Person();
+        Person person = new Person();
 
         // Ниже — намеренные попытки прямого доступа к приватным полям.
 //        person.memberName = "Alice"; // Ошибка компиляции: memberName имеет модификатор private
