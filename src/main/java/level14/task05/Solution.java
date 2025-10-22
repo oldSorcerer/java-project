50637a7eeddf8cce765ed1f9d1701b1b96eb2c61
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-10-22 17:58:59 +0300

    add solution level 14 task 05

--- src/main/java/level14/task05/Solution.java
+++ src/main/java/level14/task05/Solution.java
@@ -3,11 +3,24 @@ package ru.javarush.java.core.level14.task05;
 public class Solution {
     public static void main(String[] args) {
         // Создаем объект Car с произвольными значениями марки и года выпуска
-
+        Car car =  new Car("BMW", 2025);
 
         // Просим автомобиль "представиться"
-
+        car.displayDetails();
     }
 }
 
 // Класс Car хранит марку и год выпуска и умеет выводить информацию о себе
+class Car {
+    String carBrand;
+    int productionYear;
+
+    public Car(String carBrand, int productionYear) {
+        this.carBrand = carBrand;
+        this.productionYear = productionYear;
+    }
+
+    public void displayDetails() {
+        System.out.printf("Марка: %s, год выпуска: %s%n", carBrand, productionYear);
+    }
+}
