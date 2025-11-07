379a96b3d77cfce41c3ffc1503d8089ce2905f9c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-07 15:26:15 +0300

    add solution level 02 task 10

--- src/main/java/ru/javarush/java/core/level02/task10/Solution.java
+++ src/main/java/ru/javarush/java/core/level02/task10/Solution.java
@@ -3,15 +3,15 @@ package ru.javarush.java.core.level02.task10;
 public class Solution {
     public static void main(String[] args) {
         // Объявляем переменную типа int для хранения номера рейса
-        int number= 0;
+        int number = 123;
 
         // Объявляем переменную типа String для хранения города назначения
-        String city = null;
+        String city = "Минск";
 
         // Объединяем номер рейса и город назначения в одну строку с помощью оператора +
-
+        String string = "Рейс " + number + " до " + city;
 
         // Выводим итоговую строку на экран
-
+        System.out.println(string);
     }
 }
