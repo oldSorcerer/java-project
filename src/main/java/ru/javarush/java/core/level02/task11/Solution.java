b6920e2591e2b012c6e378aa2d302b363193aa3b
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-07 15:26:21 +0300

    add solution level 02 task 11

--- src/main/java/ru/javarush/java/core/level02/task11/Solution.java
+++ src/main/java/ru/javarush/java/core/level02/task11/Solution.java
@@ -3,12 +3,13 @@ package ru.javarush.java.core.level02.task11;
 public class Solution {
     public static void main(String[] args) {
         // Объявляем строковую переменную, содержащую год выпуска фильма
-
+        String yearStr = "1999";
 
         // Преобразуем строку в числовой тип int
-
+        int year = Integer.parseInt(yearStr);
 
         // Выводим числовое значение года на экран
+        System.out.println(year);
 
     }
 }
