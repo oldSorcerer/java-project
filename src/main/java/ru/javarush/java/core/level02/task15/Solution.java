19b4e7381cd614a234dee7b20acb24420febb02d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-07 15:27:27 +0300

    add solution level 02 task 15

--- src/main/java/ru/javarush/java/core/level02/task15/Solution.java
+++ src/main/java/ru/javarush/java/core/level02/task15/Solution.java
@@ -3,10 +3,14 @@ package ru.javarush.java.core.level02.task15;
 public class Solution {
     public static void main(String[] args) {
         // Объявляем переменные разных типов и присваиваем им произвольные значения
-
+        byte b = 0;
+        int i = 0;
+        double d = 0;
 
         // Выводим информацию о размере каждого типа данных
-
+        System.out.println("byte — 1 байт");
+        System.out.println("int — 4 байта");
+        System.out.println("double — 8 байтов");
 
     }
 }
