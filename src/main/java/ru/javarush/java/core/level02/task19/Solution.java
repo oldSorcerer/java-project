dd4e35271808876c661a6323b1e518c11bb7fffa
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-07 15:28:08 +0300

    add solution level 02 task 19

--- src/main/java/ru/javarush/java/core/level02/task19/Solution.java
+++ src/main/java/ru/javarush/java/core/level02/task19/Solution.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         // Создаем Scanner для чтения данных с клавиатуры
-
+        Scanner scanner = new Scanner(System.in);
 
         // Считываем цены двух товаров как целые числа
-
+        int price1 = scanner.nextInt();
+        int price2 = scanner.nextInt();
 
         // Складываем и выводим сумму
+        System.out.println(price1 + price2);
 
     }
 }
