ecb427987ac12239af63811363254f4b03a5deb0
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-07 15:26:34 +0300

    add solution level 02 task 13

--- src/main/java/ru/javarush/java/core/level02/task13/Solution.java
+++ src/main/java/ru/javarush/java/core/level02/task13/Solution.java
@@ -3,10 +3,16 @@ package ru.javarush.java.core.level02.task13;
 public class Solution {
     public static void main(String[] args) {
         // Объявляем переменные с информацией о персонаже
-
+        byte level = 0;
+        int goldAmount = 0;
+        double rating = 0;
+        String characterName = "";
 
         // Выводим все данные на экран
-
+        System.out.println(level);
+        System.out.println(goldAmount);
+        System.out.println(rating);
+        System.out.println(characterName);
 
     }
 }
