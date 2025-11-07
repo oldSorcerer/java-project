be8726aff8b9e3e51b4f1332d3706f511826879d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-07 15:27:53 +0300

    add solution level 02 task 17

--- src/main/java/ru/javarush/java/core/level02/task17/Solution.java
+++ src/main/java/ru/javarush/java/core/level02/task17/Solution.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         // Создаем Scanner для чтения данных с клавиатуры
-
+        Scanner scanner = new Scanner(System.in);
 
         // Считываем одну строку как пароль
-
+        String string = scanner.nextLine();
 
         // Выводим введенный пароль на экран
-
+        System.out.println(string);
     }
 }
