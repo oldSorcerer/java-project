710e028a9f1bb24c6c066d25039a025d9a09d3f1
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-11 14:54:45 +0300

    add solution level 03 task 11

--- src/main/java/ru/javarush/java/core/level03/task11/Solution.java
+++ src/main/java/ru/javarush/java/core/level03/task11/Solution.java
@@ -3,7 +3,8 @@ package ru.javarush.java.core.level03.task11;
 public class Solution {
     public static void main(String[] args) {
         // Исходные условия (можно менять для проверки разных вариантов)
-
+        boolean isSunny;
+        boolean isWeekend;
 
         // В парк идём только если одновременно солнечно И выходные
 
