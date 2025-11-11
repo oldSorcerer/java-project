40d7b1cee4849b783b4e61eac73e88bc9d543127
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-11 14:53:58 +0300

    add solution level 03 task 03

--- src/main/java/ru/javarush/java/core/level03/task03/Solution.java
+++ src/main/java/ru/javarush/java/core/level03/task03/Solution.java
@@ -3,13 +3,13 @@ package ru.javarush.java.core.level03.task03;
 public class Solution {
     public static void main(String[] args) {
         // Объявляем переменную userPassword и присваиваем ей значение "qwerty"
-
+        String userPassword = "qwerty";
 
         // Проверяем, совпадает ли пароль с ожидаемым значением "qwerty"
-//        if (/* условие */) {
-//            // Если условие истинно, выводим сообщение об успешном входе
-//
-//        }
+        if (userPassword.equals("qwerty")) {
+            // Если условие истинно, выводим сообщение об успешном входе
+            System.out.println("Вход выполнен успешно");
+        }
         // Если пароль не совпадает, блок else не используется и программа ничего не выводит
     }
 }
