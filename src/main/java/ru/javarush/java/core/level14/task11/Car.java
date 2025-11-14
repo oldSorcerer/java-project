6ea1b327fa91a030f0025215c52ebc466928e76c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-15 00:24:36 +0300

    add solution level 14 task 11

---
+++ src/main/java/ru/javarush/java/core/level14/task11/Car.java
@@ -0,0 +1,7 @@
+package ru.javarush.java.core.level14.task11;
+
+public class Car {
+    String carBrand;
+    int carYear;
+
+}
--- src/main/java/ru/javarush/java/core/level14/task11/Solution.java
+++ src/main/java/ru/javarush/java/core/level14/task11/Solution.java
@@ -6,15 +6,19 @@ package ru.javarush.java.core.level14.task11;
 public class Solution {
     public static void main(String[] args) {
         // Создаем два независимых объекта класса Car с помощью new
-
+        Car car1 = new Car();
+        Car car2 = new Car();
 
         // Инициализируем поля первого автомобиля
-
+        car1.carBrand = "Toyota";
+        car1.carYear = 2020;
 
         // Инициализируем поля второго автомобиля
-
+        car2.carBrand = "Lada";
+        car2.carYear = 2010;
 
         // Выводим информацию о каждом автомобиле в требуемом формате
-
+        System.out.printf("Марка: %s, год выпуска: %d%n", car1.carBrand, car1.carYear);
+        System.out.printf("Марка: %s, год выпуска: %d%n", car2.carBrand, car2.carYear);
     }
 }
