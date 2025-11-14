1e595f9c1c02d849d6bd98d317ead8daec25763f
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-15 00:31:16 +0300

    add solution level 16 task 01

---
+++ src/main/java/ru/javarush/java/core/level16/task01/Box.java
@@ -0,0 +1,9 @@
+package ru.javarush.java.core.level16.task01;
+
+public class Box {
+    public class Label {
+        public void printLabel() {
+            System.out.println("Box label");
+        }
+    }
+}
--- src/main/java/ru/javarush/java/core/level16/task01/Solution.java
+++ src/main/java/ru/javarush/java/core/level16/task01/Solution.java
@@ -3,13 +3,13 @@ package ru.javarush.java.core.level16.task01;
 public class Solution {
     public static void main(String[] args) {
         // Создаем коробку
-//        Box box = new Box();
+        Box box = new Box();
 
         // Создаем этикетку, связанную с этой коробкой.
         // Для создания экземпляра внутреннего класса нужен экземпляр внешнего класса.
-//        Box.Label label = box.new Label();
+        Box.Label label = box.new Label();
 
         // Вызываем метод печати этикетки
-//        label.printLabel();
+        label.printLabel();
     }
 }
