55f53f72cbe587eb5e8a6aaa6c896e557923d7ca
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-15 00:27:45 +0300

    add solution level 15 task 05

---
+++ src/main/java/ru/javarush/java/core/level15/task05/UserCard.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level15.task05;
+
+public class UserCard {
+    private String userName;
+
+    public UserCard(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+}
