972e7d3d00ce06b22c75b7e3209b2dc37b5477a0
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-15 00:30:40 +0300

    add solution level 15 task 18

---
+++ src/main/java/ru/javarush/java/core/level15/task18/ShelterPet.java
@@ -0,0 +1,12 @@
+package ru.javarush.java.core.level15.task18;
+
+public class ShelterPet {
+
+    {
+        System.out.println("Начинаем регистрацию нового питомца...");
+    }
+
+    public ShelterPet() {
+        System.out.println( "Запись о питомце успешно создана!");
+    }
+}
