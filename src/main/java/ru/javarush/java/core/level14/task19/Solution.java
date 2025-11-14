2d6a709197a40ba4d801887a1e6388928fffc6e5
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-15 00:26:26 +0300

    add solution level 14 task 19

--- src/main/java/ru/javarush/java/core/level14/task19/Solution.java
+++ src/main/java/ru/javarush/java/core/level14/task19/Solution.java
@@ -3,8 +3,15 @@ package ru.javarush.java.core.level14.task19;
 public class Solution {
     public static void main(String[] args) {
         // Запускаем "сборку" новой машины: при создании объекта сработает нестатический блок инициализации
-        
+        Car newCar = new Car();
     }
 }
 
 // "Чертёж" автомобиля
+class Car {
+    private String carModel = "Неизвестно";
+
+    {
+        System.out.println("Создаётся объект Car, model = " + carModel);
+    }
+}
