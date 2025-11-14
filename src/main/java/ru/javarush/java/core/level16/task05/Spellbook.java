af2d0133b16be84e0bb2a0bac78b43edba8d5f5a
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-15 00:32:23 +0300

    add solution level 16 task 05

---
+++ src/main/java/ru/javarush/java/core/level16/task05/Spellbook.java
@@ -0,0 +1,9 @@
+package ru.javarush.java.core.level16.task05;
+
+public class Spellbook {
+    public static class BasicCharm {
+        public void castSpell() {
+            System.out.println("Hello from static basic charm!");
+        }
+    }
+}
