9104da175ffadfeba38406a49180a400a16327e3
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-15 00:34:42 +0300

    add solution level 16 task 15

--- src/main/java/ru/javarush/java/core/level16/task15/SpecialCalculator.java
+++ src/main/java/ru/javarush/java/core/level16/task15/SpecialCalculator.java
@@ -6,9 +6,14 @@ public class SpecialCalculator {
     public void calculateAndDisplaySum(int numA, int numB) {
         // Локальный класс — временный «модуль» отображения результата.
         // Он объявлен внутри метода и имеет доступ к его параметрам (они эффективно final).
-
+        class SumResultPrinter {
+            public void printResult() {
+                System.out.println(numA + numB);
+            }
+        }
 
         // Создаем объект локального класса и сразу же выводим результат
-
+        SumResultPrinter sumResultPrinter = new SumResultPrinter();
+        sumResultPrinter.printResult();
     }
 }
