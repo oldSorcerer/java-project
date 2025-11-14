0e9b7750c882060dbeccdbddde50c9c9a3538ed5
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-15 00:35:11 +0300

    add solution level 16 task 19

--- src/main/java/ru/javarush/java/core/level16/task19/Solution.java
+++ src/main/java/ru/javarush/java/core/level16/task19/Solution.java
@@ -1,18 +1,27 @@
 package ru.javarush.java.core.level16.task19;
 
 // Универсальная пара данных: класс для хранения двух связанных элементов одного типа
-class DataPair {
+class DataPair<T> {
     // Два приватных поля обобщённого типа T
-
+    private T firstElement;
+    private T secondElement;
 
     // Конструктор принимает два значения и инициализирует поля
 
+    public DataPair(T firstElement, T secondElement) {
+        this.firstElement = firstElement;
+        this.secondElement = secondElement;
+    }
 
     // Возвращаем первый элемент пары
-
+    public T getFirst() {
+        return firstElement;
+    }
 
     // Возвращаем второй элемент пары
-
+    public T getSecond() {
+        return secondElement;
+    }
 }
 
 public class Solution {
