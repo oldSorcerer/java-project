31855872f9ab0658a1ef70a1a22a191ea4943ec8
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-15 00:32:30 +0300

    add solution level 16 task 06

--- src/main/java/ru/javarush/java/core/level16/task06/Vault.java
+++ src/main/java/ru/javarush/java/core/level16/task06/Vault.java
@@ -5,7 +5,12 @@ public class Vault {
     private static int securityLevel = 100;
 
     // Публичный статический вложенный класс — не требует объекта Vault для создания
+    public static class SecurityDisplayUnit {
 
+        public void showSecurityLevel() {
+            System.out.println(securityLevel);
+        }
+    }
 
     public static void main(String[] args) {
         // Создаем объект вложенного класса без создания объекта Vault
