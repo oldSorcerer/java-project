5acd0e764a17038bbcff2de688f344dc0399a221
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-15 00:35:23 +0300

    add solution level 16 task 20

--- src/main/java/ru/javarush/java/core/level16/task20/Solution.java
+++ src/main/java/ru/javarush/java/core/level16/task20/Solution.java
@@ -5,7 +5,10 @@ public class Solution {
     // Публичный статический (вложенный) класс с утилитарным методом
     public static class DevUtils {
         // Универсальный метод с дженериком: принимает параметр любого типа T
-
+        public static <T> void printTwice(T value) {
+            System.out.println(value);
+            System.out.println(value);
+        }
     }
 
     public static void main(String[] args) {
