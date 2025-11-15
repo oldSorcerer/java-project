3cbdec7c4dd37651f60024f07709eed1c723ad7e
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-16 00:39:49 +0300

    add solution level 18 task 06

---
+++ src/main/java/ru/javarush/java/core/level18/task06/Multiplier.java
@@ -0,0 +1,12 @@
+package ru.javarush.java.core.level18.task06;
+
+public class Multiplier {
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+}
