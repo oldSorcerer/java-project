ea0472da7b7a834ddea446fe150edfc4f12d4f08
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-16 00:39:42 +0300

    add solution level 18 task 05

---
+++ src/main/java/ru/javarush/java/core/level18/task05/MessagePrinter.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level18.task05;
+
+public class MessagePrinter {
+
+    public void printMessage(String string) {
+        System.out.println(string);
+    }
+
+    public void printMessage(int a, int b) {
+        System.out.println(a + b);
+    }
+    
+}
