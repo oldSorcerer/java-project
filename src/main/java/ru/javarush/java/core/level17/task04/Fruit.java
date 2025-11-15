e869c195c620f305fff55e4ebc7946f7a43d80e9
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-16 00:36:56 +0300

    add solution level 17 task 04

---
+++ src/main/java/ru/javarush/java/core/level17/task04/Fruit.java
@@ -0,0 +1,9 @@
+package ru.javarush.java.core.level17.task04;
+
+public class Fruit {
+    public String fruitColor;
+
+    public  void printColor() {
+        System.out.printf("Цвет: %s%n", fruitColor);
+    }
+}
--- src/main/java/ru/javarush/java/core/level17/task04/Solution.java
+++ src/main/java/ru/javarush/java/core/level17/task04/Solution.java
@@ -3,15 +3,15 @@ package ru.javarush.java.core.level17.task04;
 public class Solution {
     public static void main(String[] args) {
         // Создаем объекты яблока и банана
-//        Apple apple = new Apple();
-//        Banana banana = new Banana();
+        Apple apple = new Apple();
+        Banana banana = new Banana();
 
         // Задаем цвета для каждого фрукта (поле унаследовано от Fruit)
-//        apple.fruitColor = "красный";
-//        banana.fruitColor = "жёлтый";
+        apple.fruitColor = "красный";
+        banana.fruitColor = "жёлтый";
 
         // Выводим цвет каждого фрукта
-//        apple.printColor();
-//        banana.printColor();
+        apple.printColor();
+        banana.printColor();
     }
 }
