ac0cf17eb257c9f4ab22515446079feabb8cc569
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-16 00:38:13 +0300

    add solution level 17 task 14

---
+++ src/main/java/ru/javarush/java/core/level17/task14/Car.java
@@ -0,0 +1,8 @@
+package ru.javarush.java.core.level17.task14;
+
+public class Car extends Vehicle {
+
+    public void beep() {
+        System.out.println("Машина сигналит: Бип-бип!");
+    }
+}
---
+++ src/main/java/ru/javarush/java/core/level17/task14/Vehicle.java
@@ -0,0 +1,7 @@
+package ru.javarush.java.core.level17.task14;
+
+public class Vehicle {
+    public void move() {
+        System.out.println("Транспорт движется.");
+    }
+}
