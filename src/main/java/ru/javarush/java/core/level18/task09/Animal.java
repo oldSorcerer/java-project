df6bd2a94cde9a0669835bbbbd3beccfab7f12bc
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-16 00:40:09 +0300

    add solution level 18 task 09

---
+++ src/main/java/ru/javarush/java/core/level18/task09/Animal.java
@@ -0,0 +1,8 @@
+package ru.javarush.java.core.level18.task09;
+
+public class Animal {
+
+    public void makeSound() {
+        System.out.println("Животное издаёт звук");
+    }
+}
---
+++ src/main/java/ru/javarush/java/core/level18/task09/Dog.java
@@ -0,0 +1,8 @@
+package ru.javarush.java.core.level18.task09;
+
+public class Dog extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Гав!");
+    }
+}
