4f513cae2201b15f8939c7a0ec9d7e4961f7d25d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-16 00:38:40 +0300

    add solution level 17 task 18

---
+++ src/main/java/ru/javarush/java/core/level17/task18/Child.java
@@ -0,0 +1,9 @@
+package ru.javarush.java.core.level17.task18;
+
+public class Child extends Parent {
+
+    public void demo() {
+//        System.out.println(familySecret);
+//        this.familySecret;
+    }
+}
---
+++ src/main/java/ru/javarush/java/core/level17/task18/Parent.java
@@ -0,0 +1,9 @@
+package ru.javarush.java.core.level17.task18;
+
+public class Parent {
+    private  int familySecret = 42;
+
+    public void printSecret() {
+        System.out.println(familySecret);
+    }
+}
