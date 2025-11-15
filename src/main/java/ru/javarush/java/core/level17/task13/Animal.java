04c683f6b9bd7587dec68a742751368f5752d5d4
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-16 00:38:06 +0300

    add solution level 17 task 13

---
+++ src/main/java/ru/javarush/java/core/level17/task13/Animal.java
@@ -0,0 +1,9 @@
+package ru.javarush.java.core.level17.task13;
+
+public class Animal {
+    String petName;
+
+    public void eat() {
+        System.out.printf("%s ест.%n", petName);
+    }
+}
---
+++ src/main/java/ru/javarush/java/core/level17/task13/Dog.java
@@ -0,0 +1,5 @@
+package ru.javarush.java.core.level17.task13;
+
+public class Dog extends Animal {
+
+}
