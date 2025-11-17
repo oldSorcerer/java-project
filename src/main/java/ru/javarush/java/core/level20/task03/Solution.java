5f4ac284eb15e99cd6b68be1eca990dd5ac2da84
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-17 15:45:20 +0300

    add solution level 20 task 03

---
+++ src/main/java/ru/javarush/java/core/level20/task03/Solution.java
@@ -0,0 +1,9 @@
+package ru.javarush.java.core.level20.task03;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем экземпляр калькулятора и выводим значение PI
+        Calculator calculator = new Calculator();
+        calculator.printPi(); // ожидаемый вывод: 3.14159
+    }
+}
