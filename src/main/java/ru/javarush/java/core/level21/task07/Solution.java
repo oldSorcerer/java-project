e0096ded39f9fa87193380a748dc908a1913cf7b
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-17 15:48:55 +0300

    add solution level 21 task 07

---
+++ src/main/java/ru/javarush/java/core/level21/task07/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level21.task07;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем список строк с фруктами
+
+
+        // Сортируем список по алфавиту, используя ссылку на метод сравнения строк
+
+
+        // Выводим каждый фрукт на новой строке
+
+    }
+}
