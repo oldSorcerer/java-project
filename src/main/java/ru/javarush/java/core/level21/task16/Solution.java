e4d07fc86daa44a5f9d20e2ab3ab8114230b5d88
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-17 15:50:44 +0300

    add solution level 21 task 16

---
+++ src/main/java/ru/javarush/java/core/level21/task16/Solution.java
@@ -0,0 +1,9 @@
+package ru.javarush.java.core.level21.task16;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Демонстрация работы: оба вызова печатают сообщения в едином формате
+        Logger.info("Start");
+        Logger.error("Failure");
+    }
+}
