76bc106acb6ac7bfada183c797b301156a17eab5
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-17 15:46:52 +0300

    add solution level 20 task 15

---
+++ src/main/java/ru/javarush/java/core/level20/task15/Solution.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level20.task15;
+
+import java.util.function.Predicate;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Предикат проверяет, что длина строки больше 5 символов
+
+
+        // Демонстрация работы на двух примерах
+
+    }
+}
