cb41442680128a84aecc995c372cd1d638dd62c0
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-17 15:48:09 +0300

    add solution level 21 task 01

---
+++ src/main/java/ru/javarush/java/core/level21/task01/Solution.java
@@ -0,0 +1,12 @@
+package ru.javarush.java.core.level21.task01;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Используем стандартный функциональный интерфейс Runnable:
+        // у него нет параметров и нет возвращаемого значения.
+
+
+        // Вызываем действие — сообщение будет напечатано на экран.
+
+    }
+}
