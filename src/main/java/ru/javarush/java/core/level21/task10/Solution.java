69dfd4b30f4dce40a86e3af8f46e060ac96df13e
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-17 15:49:18 +0300

    add solution level 21 task 10

---
+++ src/main/java/ru/javarush/java/core/level21/task10/Solution.java
@@ -0,0 +1,10 @@
+package ru.javarush.java.core.level21.task10;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект Dog и вызываем его методы
+        Dog dog = new Dog();
+        dog.voice();
+        dog.sleep();
+    }
+}
