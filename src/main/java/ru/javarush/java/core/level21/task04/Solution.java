028cf708e584e872eafc097524bc6263c14a2776
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-17 15:48:34 +0300

    add solution level 21 task 04

---
+++ src/main/java/ru/javarush/java/core/level21/task04/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level21.task04;
+
+import java.util.function.IntFunction;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Фиксированный префикс для всех отчётных строк
+
+
+        // Лямбда-выражение — "функция форматирования":
+
+
+        // Вызов функции с аргументом 7 и вывод результата
+
+    }
+}
