87a0c667b502b701dd4cc1597cebd93b935e63cc
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-17 15:46:27 +0300

    add solution level 20 task 11

---
+++ src/main/java/ru/javarush/java/core/level20/task11/Solution.java
@@ -0,0 +1,9 @@
+package ru.javarush.java.core.level20.task11;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект и вызываем его метод doAction()
+        MultiAction action = new MultiAction();
+        action.doAction();
+    }
+}
