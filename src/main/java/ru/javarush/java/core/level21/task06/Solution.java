a12a6fd51ea8a2edee78bb21e5f85adc124fb7f9
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-17 15:48:49 +0300

    add solution level 21 task 06

---
+++ src/main/java/ru/javarush/java/core/level21/task06/Solution.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level21.task06;
+
+import java.util.function.Consumer;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем "отправителя" как Consumer<String>
+
+
+        // Используем отправителя для вывода сообщения (без прямого вызова System.out.println)
+
+    }
+}
