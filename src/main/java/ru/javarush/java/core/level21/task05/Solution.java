d3e6877fad29ea0de3d3d784a21a972df5277091
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-17 15:48:42 +0300

    add solution level 21 task 05

---
+++ src/main/java/ru/javarush/java/core/level21/task05/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level21.task05;
+
+import java.util.function.IntFunction;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем "конвертер" через ссылку на статический метод Integer.toHexString(int)
+
+
+        // Применяем конвертер к числу 255
+
+
+        // Выводим шестнадцатеричное представление на экран
+
+    }
+}
