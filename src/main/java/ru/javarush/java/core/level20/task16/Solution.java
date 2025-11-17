4144092e6d49d0b77932e59483b465e1ed8f2082
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-17 15:46:59 +0300

    add solution level 20 task 16

---
+++ src/main/java/ru/javarush/java/core/level20/task16/Solution.java
@@ -0,0 +1,11 @@
+package ru.javarush.java.core.level20.task16;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Лямбда-реализация: преобразует строку к нижнему регистру
+
+
+        // Вызываем default-метод для строки "JAVA ROCKS"
+
+    }
+}
