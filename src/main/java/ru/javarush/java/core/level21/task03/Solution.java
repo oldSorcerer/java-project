ebc00f8894f51a5bb1c13df765b3ee5581fc7e36
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-17 15:48:27 +0300

    add solution level 21 task 03

---
+++ src/main/java/ru/javarush/java/core/level21/task03/Solution.java
@@ -0,0 +1,22 @@
+package ru.javarush.java.core.level21.task03;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Исходный список результатов спортсменов
+
+
+        // Компаратор-лямбда: сравниваем числа только по их абсолютному значению (знак игнорируется)
+
+
+        // Сортируем список по возрастанию абсолютной величины
+
+
+        // Выводим отсортированный список
+
+    }
+}
