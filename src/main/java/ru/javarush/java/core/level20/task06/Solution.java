7a97681d82172647325b57705d9db12def057235
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-17 15:45:48 +0300

    add solution level 20 task 06

---
+++ src/main/java/ru/javarush/java/core/level20/task06/Solution.java
@@ -0,0 +1,9 @@
+package ru.javarush.java.core.level20.task06;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Пример использования:
+        Student student = new Student("Иван Петров");
+        student.printInfo(); // выведет имя и ID
+    }
+}
