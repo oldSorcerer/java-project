ae7a7908fd64fa716a437123b35b299e06375013
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-17 15:48:18 +0300

    add solution level 21 task 02

---
+++ src/main/java/ru/javarush/java/core/level21/task02/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level21.task02;
+
+import java.util.function.Consumer;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Обработчик текста: принимает строку и печатает её в верхнем регистре.
+        // Используем стандартный функциональный интерфейс Consumer<String>.
+
+
+        // Проверка работы обработчика на примере:
+
+    }
+}
