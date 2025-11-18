748d78974b04e9706fe0d8301afc7bcea2f07d4c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-18 15:44:07 +0300

    add solution level 23 task 18

---
+++ src/main/java/ru/javarush/java/core/level23/task18/Solution.java
@@ -0,0 +1,15 @@
+package ru.javarush.java.core.level23.task18;
+
+public class Solution {
+    // Осмысленные имена полей
+
+
+    // Переименованный метод: чётко указывает, что выводит сообщение
+
+
+    public static void main(String[] args) {
+        // Создаем объект и вызываем метод для демонстрации результата
+        Solution app = new Solution();
+        app.printMessage();
+    }
+}
