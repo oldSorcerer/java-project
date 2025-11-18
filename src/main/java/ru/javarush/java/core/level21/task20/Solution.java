32775f258001b0f1fe21d6daa5634affe310c2e2
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-18 15:34:42 +0300

    add solution level 21 task 20

---
+++ src/main/java/ru/javarush/java/core/level21/task20/Solution.java
@@ -0,0 +1,9 @@
+package ru.javarush.java.core.level21.task20;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Используем try-with-resources: ресурс будет закрыт автоматически
+
+
+    }
+}
