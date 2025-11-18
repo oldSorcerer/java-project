db1d88bbc4b52451177c079260a078dffa0652f9
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-18 15:45:11 +0300

    add solution level 24 task 04

---
+++ src/main/java/ru/javarush/java/core/level24/task04/Solution.java
@@ -0,0 +1,22 @@
+package ru.javarush.java.core.level24.task04;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Solution {
+    public static void main(String[] args) {
+        try {
+            // Имитация поиска отчёта: намеренно бросаем исключение
+
+        } catch (... e) {
+            // Самый специфичный план: файл отсутствует
+
+        } catch (... e) {
+            // Более общий план: проблемы ввода-вывода
+
+        } catch (... e) {
+            // Самый общий план: любая другая непредвиденная ситуация
+
+        }
+    }
+}
