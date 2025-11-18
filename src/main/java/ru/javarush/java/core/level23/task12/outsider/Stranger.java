dd51591eb4e5308626439c63713981815684f6bb
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-18 15:43:12 +0300

    add solution level 23 task 12

---
+++ src/main/java/ru/javarush/java/core/level23/task12/outsider/Stranger.java
@@ -0,0 +1,12 @@
+package ru.javarush.java.core.level23.task12.outsider;
+
+import ru.javarush.java.core.level23.task12.zoo.Animal;
+
+public class Stranger {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+
+        // Ниже — преднамеренная ошибка компиляции:
+        
+    }
+}
