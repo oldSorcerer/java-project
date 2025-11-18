6b26b50ab17fe6eebfd7b1da411421c477679c5c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-18 15:41:58 +0300

    add solution level 23 task 04

---
+++ src/main/java/ru/javarush/java/core/level23/task04/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level23.task04;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаём товар, обязательно указывая имя в конструкторе
+
+
+        // Выводим имя товара на экран, подтверждая успешное создание
+
+
+        // Пример неправильного использования:
+
+    }
+}
