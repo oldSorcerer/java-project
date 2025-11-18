b66672479fdf3b94ea213f3c38793f9ccaade00c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-18 15:44:15 +0300

    add solution level 23 task 19

---
+++ src/main/java/ru/javarush/java/core/level23/task19/Solution.java
@@ -0,0 +1,10 @@
+package ru.javarush.java.core.level23.task19;
+
+public class Solution {
+    // Константа налоговой ставки. Так понятнее и безопаснее, чем "магическое число" 0.13 в коде.
+
+
+    public static void main(String[] args) {
+
+    }
+}
