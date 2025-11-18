74dded7fa29bd880f66adb1fc9e13393386b7746
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-18 15:43:04 +0300

    add solution level 23 task 11

---
+++ src/main/java/ru/javarush/java/core/level23/task11/Solution.java
@@ -0,0 +1,18 @@
+package ru.javarush.java.core.level23.task11;
+
+// Класс, описывающий пользователя
+class User {
+    // Приватное строковое поле: доступно только внутри класса User
+    private String userPassword;
+}
+
+// Сервис для работы с пользователями
+class UserService {
+    public static void main(String[] args) {
+        // Создаем объект пользователя
+
+
+        // Попытка напрямую присвоить значение приватному полю из другого класса:
+
+    }
+}
