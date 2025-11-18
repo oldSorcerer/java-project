f8a053b37489fb4d7ff43facc142f5f4a60a268b
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-18 15:43:55 +0300

    add solution level 23 task 17

---
+++ src/main/java/ru/javarush/java/core/level23/task17/Solution.java
@@ -0,0 +1,11 @@
+package ru.javarush.java.core.level23.task17;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем целочисленную переменную и присваиваем ей значение
+
+
+        // Аккуратно оформленный блок if-else: проверяем, положительное ли число
+
+    }
+}
