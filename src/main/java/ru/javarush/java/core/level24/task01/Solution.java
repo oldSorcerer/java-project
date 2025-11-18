038cec41f19b610cc4064ec79179308dbdf50101
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-18 15:44:42 +0300

    add solution level 24 task 01

---
+++ src/main/java/ru/javarush/java/core/level24/task01/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level24.task01;
+
+import java.io.IOException;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем две "проблемы": сетевую (IOException) и пропажу данных (NullPointerException)
+
+
+        // Для каждого исключения получаем непосредственный класс-родитель через рефлексию
+
+
+        // Выводим имена родительских классов
+
+    }
+}
