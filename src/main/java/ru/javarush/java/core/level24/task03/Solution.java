415df7c8575389b94d2edfa7d4ea9e4b9c0eefe4
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-18 15:44:59 +0300

    add solution level 24 task 03

---
+++ src/main/java/ru/javarush/java/core/level24/task03/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level24.task03;
+
+import java.io.FileNotFoundException;
+
+public class Solution {
+    public static void main(String[] args) {
+        // "Неожиданный сбой": RuntimeException — непроверяемое (unchecked) исключение
+
+
+        // "Потерянный файл конфигурации": FileNotFoundException — проверяемое (checked) исключение
+
+
+        // Выводим классификацию исключений
+
+    }
+}
