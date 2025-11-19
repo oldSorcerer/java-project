bc670018fa77c01856307a1867c9e85c885f43ee
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-19 17:20:32 +0300

    add solution level 25 task 04

---
+++ src/main/java/ru/javarush/java/core/level25/task04/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level25.task04;
+
+// Интерфейс с единственным абстрактным методом
+interface Pressable {
+    void activateEngine();
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект интерфейса Pressable "на лету" с помощью анонимного класса
+
+
+        // "Нажимаем" кнопку: вызываем метод активации
+
+    }
+}
