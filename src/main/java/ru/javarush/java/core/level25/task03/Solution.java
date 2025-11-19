cf9597c7ea7d66ce7445550e42b0ce61520800b4
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-19 17:20:25 +0300

    add solution level 25 task 03

---
+++ src/main/java/ru/javarush/java/core/level25/task03/Solution.java
@@ -0,0 +1,9 @@
+package ru.javarush.java.core.level25.task03;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Вызываем статический метод вложенного класса по полному имени,
+        // не создавая объект внешнего класса Library
+
+    }
+}
