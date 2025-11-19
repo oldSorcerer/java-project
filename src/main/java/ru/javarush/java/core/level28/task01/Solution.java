154f019b41154e9c4f9d4b4b97d7639a9ffde0e9
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-19 17:32:01 +0300

    add solution level 28 task 01

---
+++ src/main/java/ru/javarush/java/core/level28/task01/Solution.java
@@ -0,0 +1,19 @@
+package ru.javarush.java.core.level28.task01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем изменяемый список с начальными значениями
+
+
+        // Обходим список с помощью Iterator, чтобы безопасно удалять элементы во время обхода
+
+
+        // Выводим обновленный список без отрицательных чисел
+
+    }
+}
