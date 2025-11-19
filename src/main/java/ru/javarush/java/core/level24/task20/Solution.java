58a9355c7b9f5583136d841b7229a1386158ea07
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-19 17:19:31 +0300

    add solution level 24 task 20

---
+++ src/main/java/ru/javarush/java/core/level24/task20/Solution.java
@@ -0,0 +1,18 @@
+package ru.javarush.java.core.level24.task20;
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        // try-with-resources: одновременно открываем "читателя" и "писателя".
+
+
+
+            // Читаем исходный файл построчно и сразу записываем каждую строку в целевой
+
+
+
+            // Если произошла ошибка при чтении/записи — выводим требуемое сообщение
+
+    }
+}
