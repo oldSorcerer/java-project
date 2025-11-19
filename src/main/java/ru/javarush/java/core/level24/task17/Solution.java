2e99abbbf479ad618c9e920e6c54e564f8f13ca8
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-19 17:19:05 +0300

    add solution level 24 task 17

---
+++ src/main/java/ru/javarush/java/core/level24/task17/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level24.task17;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Используем try-with-resources, чтобы поток автоматически закрылся
+
+            // Читаем только первую строку файла — заголовок
+
+            // На любую ошибку чтения выводим сообщение об ошибке
+
+
+    }
+}
