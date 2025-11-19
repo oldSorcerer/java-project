2ce55384819a97eaf2a1214300421b891e177690
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-19 17:21:43 +0300

    add solution level 25 task 08

---
+++ src/main/java/ru/javarush/java/core/level25/task08/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level25.task08;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Простейшая реализация Renderable через анонимный класс
+
+
+        // Вызываем обязательный метод отрисовки
+
+
+        // Демонстрация default-метода интерфейса
+
+
+        // Вызов статического метода напрямую через интерфейс
+
+    }
+}
