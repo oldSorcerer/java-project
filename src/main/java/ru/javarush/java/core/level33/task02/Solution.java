34d10add2425dce73fc8f374ab303f11b51c2330
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-20 17:48:24 +0300

    add solution level 33 task 02

---
+++ src/main/java/ru/javarush/java/core/level33/task02/Solution.java
@@ -0,0 +1,15 @@
+package ru.javarush.java.core.level33.task02;
+
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Исходный список названий заклинаний
+        List<String> ancientSpells = List.of("Java", "Stream", "API", "IntStream");
+
+        // Преобразуем Stream<String> в IntStream с помощью mapToInt,
+        // чтобы получить примитивные int (без автоупаковки в Integer),
+        // и печатаем каждую длину на новой строке.
+
+    }
+}
