30886f649f26051342626d5095f6cd1cf8945fb1
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-20 17:48:18 +0300

    add solution level 33 task 01

---
+++ src/main/java/ru/javarush/java/core/level33/task01/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level33.task01;
+
+import java.util.stream.IntStream;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Массив сумм отдельных продаж за день
+        int[] dailySalesFigures = {10, 20, 30, 40, 50};
+
+        // Примитивный стрим IntStream (без boxing) + терминальная операция sum()
+
+
+        // Выводим итоговую сумму выручки
+        System.out.println(total);
+    }
+}
