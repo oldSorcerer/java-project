b2470f66c39bd9ea1b53fa05c1c8477fe06ad48a
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-20 17:35:05 +0300

    add solution level 28 task 08

---
+++ src/main/java/ru/javarush/java/core/level28/task08/Solution.java
@@ -0,0 +1,21 @@
+package ru.javarush.java.core.level28.task08;
+
+import java.util.EnumMap;
+
+// Перечисление дней недели
+enum Day {
+    MON, TUE, WED, THU, FRI, SAT, SUN
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        // Специализированная карта для enum-ключей: работает быстро и экономит память
+
+
+        // Заполняем цены согласно условию
+
+
+        // Получаем цену на субботу и выводим на экран
+
+    }
+}
