2a0b040e8ca98c579446355b6572096ea1c23233
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-20 17:36:58 +0300

    add solution level 30 task 01

---
+++ src/main/java/ru/javarush/java/core/level30/task01/Solution.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level30.task01;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаём последовательность уровней как массив целых чисел
+        int[] levels = {1, 2, 3, 4, 5};
+
+        // Преобразуем массив в поток и обрабатываем каждый элемент отдельно с помощью forEach
+
+    }
+}
