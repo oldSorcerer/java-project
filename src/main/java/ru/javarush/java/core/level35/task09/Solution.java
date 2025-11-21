a4eb71e884ef9d0581cc5181260a3f9afee126db
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-21 12:07:57 +0300

    add solution level 34 task 09

---
+++ src/main/java/ru/javarush/java/core/level35/task09/Solution.java
@@ -0,0 +1,19 @@
+package ru.javarush.java.core.level35.task09;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        // Относительный путь от текущей рабочей директории до годовой папки
+
+
+        // Создаём всю цепочку директорий сразу.
+        // Если какие-то папки уже есть — ошибки не будет.
+
+
+        // Получаем абсолютный путь к самой глубокой папке ("2024") и выводим его
+
+    }
+}
