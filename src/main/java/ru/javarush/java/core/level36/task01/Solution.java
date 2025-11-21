820c6e0e8411c0a3f5e44eefe981f65cd1e075ea
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-21 12:08:28 +0300

    add solution level 36 task 01

---
+++ src/main/java/ru/javarush/java/core/level36/task01/Solution.java
@@ -0,0 +1,20 @@
+package ru.javarush.java.core.level36.task01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Path scrollPath = Paths.get("древний_свиток.txt");
+
+        // Считываем только первую строку — заголовок/основную мысль свитка
+
+
+        // Выводим её на экран (если файл пуст, ничего не выведется)
+
+    }
+}
