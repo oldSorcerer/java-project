824807cd9fd44e183a35426e25246e46a4eba478
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-21 12:07:00 +0300

    add solution level 34 task 02

---
+++ src/main/java/ru/javarush/java/core/level35/task02/Solution.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level35.task02;
+
+import java.io.File;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Работаем с файлом "my_game_save.dat" в текущей (рабочей) директории
+
+
+        // Проверяем существование и то, что это именно файл, а не папка
+
+    }
+}
