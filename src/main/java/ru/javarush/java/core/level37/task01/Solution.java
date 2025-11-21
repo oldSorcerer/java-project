0d18c005267151e8e8570d2dd15cc399f3e82df0
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-21 12:10:23 +0300

    add solution level 37 task 01

---
+++ src/main/java/ru/javarush/java/core/level37/task01/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level37.task01;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        // Сообщение, которое нужно закодировать
+
+
+        // Преобразуем строку в массив байтов в кодировке UTF-8
+
+
+        // Выводим каждый байт на новой строке
+
+    }
+}
