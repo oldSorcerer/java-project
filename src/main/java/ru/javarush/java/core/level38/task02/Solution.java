f813197699fc2a067d916d049666b3d012ea717f
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-21 12:12:02 +0300

    add solution level 38 task 02

---
+++ src/main/java/ru/javarush/java/core/level38/task02/Solution.java
@@ -0,0 +1,12 @@
+package ru.javarush.java.core.level38.task02;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Solution {
+    public static void main(String[] args) {
+        // try-with-resources автоматически закроет BufferedWriter после использования
+
+    }
+}
