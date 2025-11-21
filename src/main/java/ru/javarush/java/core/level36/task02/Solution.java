b5ad170e53730402d2496ca5e6de72f0a70dda6c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-21 12:08:35 +0300

    add solution level 36 task 02

---
+++ src/main/java/ru/javarush/java/core/level36/task02/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level36.task02;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        // Имя файла из условия. При необходимости можно заменить на "output.txt".
+        Path file = Path.of("анналы_истины.txt");
+
+        // Записываем ровно одну строку без перевода строки в конце
+
+    }
+}
