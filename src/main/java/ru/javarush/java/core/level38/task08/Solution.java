f1f02f1bb3052f5b62078671945f111e3161f298
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-21 12:14:08 +0300

    add solution level 38 task 08

---
+++ src/main/java/ru/javarush/java/core/level38/task08/Solution.java
@@ -0,0 +1,23 @@
+package ru.javarush.java.core.level38.task08;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем Scanner для чтения пути к директории из консоли
+        Scanner console = new Scanner(System.in);
+        String dirPath = console.nextLine().trim();
+
+        // Создаем объект Path на основе введенной строки
+
+
+        // Проверяем, доступна ли директория для записи
+
+
+
+        // Выводим результат проверки: true или false
+
+    }
+}
