5713ff52a46a51cf842c91f0986279f9637058c5
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-21 12:09:41 +0300

    add solution level 36 task 09

---
+++ src/main/java/ru/javarush/java/core/level36/task09/Solution.java
@@ -0,0 +1,12 @@
+package ru.javarush.java.core.level36.task09;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        // try-with-resources автоматически закроет BufferedWriter после завершения блока
+
+    }
+}
