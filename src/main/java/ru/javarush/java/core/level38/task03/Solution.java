2d4595a61b0ae5a430a73883cfc548d643a8caff
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-21 12:12:09 +0300

    add solution level 38 task 03

---
+++ src/main/java/ru/javarush/java/core/level38/task03/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level38.task03;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект Path, указывающий на директорию "data" в текущей (рабочей) директории
+
+
+        // Проверяем, что путь существует и это именно директория (а не файл)
+
+
+        // Выводим результат проверки
+
+    }
+}
