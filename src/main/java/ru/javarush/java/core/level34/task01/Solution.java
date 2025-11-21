188a65b7a6f21220525040b441ea40b0a5d260be
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-21 12:05:00 +0300

    add solution level 34 task 01

---
+++ src/main/java/ru/javarush/java/core/level34/task01/Solution.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level34.task01;
+
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем неизменяемый список базовых цветов с помощью List.of
+        List<String> colors = List.of("красный", "синий");
+
+        // Пытаемся добавить новый цвет в неизменяемый список
+
+    }
+}
