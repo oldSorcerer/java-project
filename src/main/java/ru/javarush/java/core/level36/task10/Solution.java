525054bbdad3a01fb697b4e7cf738723b624485f
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-21 12:10:11 +0300

    add solution level 36 task 10

---
+++ src/main/java/ru/javarush/java/core/level36/task10/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level36.task10;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        int wordCount = 0; // переменная для хранения количества слов
+
+        // Используем try-with-resources для автоматического закрытия Scanner
+
+
+        // Выводим общее число слов на экран
+        System.out.println(wordCount);
+    }
+}
