83f402379dc9ca31810a180101a87a778aaac2ee
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-21 12:06:50 +0300

    add solution level 34 task 01

---
+++ src/main/java/ru/javarush/java/core/level35/task01/Solution.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level35.task01;
+
+import java.io.File;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект File, представляющий файл "дневник.txt" в текущей директории
+
+
+        // Проверяем существование файла с помощью метода exists()
+
+    }
+}
