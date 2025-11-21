aa8ac3f9e1bbd8dc92951206a8d511c9f2f0d1a6
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-21 12:05:09 +0300

    add solution level 34 task 02

---
+++ src/main/java/ru/javarush/java/core/level34/task02/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level34.task02;
+
+import java.util.List;
+import java.util.Set;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Демонстрация запрета null в неизменяемых списках List.of
+
+
+        // Демонстрация запрета дубликатов в неизменяемых множествах Set.of
+
+    }
+}
