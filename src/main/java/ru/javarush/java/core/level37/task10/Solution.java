f2bd2797b757a33de584948b6e95f8ccfb8eb487
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-21 12:11:38 +0300

    add solution level 37 task 10

---
+++ src/main/java/ru/javarush/java/core/level37/task10/Solution.java
@@ -0,0 +1,23 @@
+package ru.javarush.java.core.level37.task10;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Пути к файлам: исходный (ISO-8859-1) и целевой (UTF-8)
+        String inputDataPath = "input_iso_8859_1.txt";
+        String outputDataPath = "output_utf8.txt";
+
+        // Явно задаём исходную и целевую кодировки
+
+
+        // try-with-resources гарантирует закрытие файлов даже при ошибках
+
+    }
+}
