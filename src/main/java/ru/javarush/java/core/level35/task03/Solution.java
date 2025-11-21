3be0dc9aa3e3066e6eab189a809d9b9c84d90e63
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-21 12:07:08 +0300

    add solution level 34 task 03

---
+++ src/main/java/ru/javarush/java/core/level35/task03/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level35.task03;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект Path, указывающий на директорию "game_assets"
+
+
+        // Проверяем, существует ли путь и является ли он директорией
+
+
+        // Выводим однозначный результат
+
+    }
+}
