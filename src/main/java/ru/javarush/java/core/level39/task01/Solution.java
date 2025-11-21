4350ebab3d8329e76316dd56ae79c7c5abd82e7c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-21 12:14:41 +0300

    add solution level 39 task 01

---
+++ src/main/java/ru/javarush/java/core/level39/task01/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level39.task01;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем путь к директории "myfolder" (относительно текущей рабочей директории)
+
+
+        // Получаем только непосредственные элементы директории через Files.newDirectoryStream(...)
+        // try-with-resources гарантирует закрытие потока даже при ошибке
+
+    }
+}
