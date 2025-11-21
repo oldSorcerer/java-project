15f0074053b417812edf848d98d552980e0a1d4d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-21 12:12:24 +0300

    add solution level 38 task 05

---
+++ src/main/java/ru/javarush/java/core/level38/task05/Solution.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level38.task05;
+
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Читаем бинарный файл с помощью DataInputStream
+
+    }
+}
