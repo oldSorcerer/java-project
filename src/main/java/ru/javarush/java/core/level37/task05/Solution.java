f2a3591c2ce76a29067e4013372b2bba303fda14
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-21 12:10:58 +0300

    add solution level 37 task 05

---
+++ src/main/java/ru/javarush/java/core/level37/task05/Solution.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level37.task05;
+
+import java.nio.charset.Charset;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Определяем системную кодировку по умолчанию средствами стандартной библиотеки
+        
+
+        // Выводим только значение кодировки, без дополнительного текста
+
+    }
+}
