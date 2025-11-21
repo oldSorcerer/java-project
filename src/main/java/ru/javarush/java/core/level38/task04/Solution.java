a77808e52e9c6a34878ea37ec84cdbb316ae47cf
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-21 12:12:17 +0300

    add solution level 38 task 04

---
+++ src/main/java/ru/javarush/java/core/level38/task04/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level38.task04;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        // Создаем объект Path, указывающий на папку "backup" в текущей директории
+
+
+        // Проверяем существование папки с помощью Files.exists(Path)
+
+    }
+}
