8fbce9a53ee7bc9cf6c28fbc233eac08b3e8d851
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-21 12:11:55 +0300

    add solution level 38 task 01

---
+++ src/main/java/ru/javarush/java/core/level38/task01/Solution.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level38.task01;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Пытаемся открыть файл "missing.txt" с помощью FileReader.
+        // Используем try-with-resources, чтобы гарантировать закрытие ресурса.
+
+    }
+}
