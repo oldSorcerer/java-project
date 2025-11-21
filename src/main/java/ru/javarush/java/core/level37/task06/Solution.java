e23804bd1da04199d972f6b781051a39440d88b9
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-21 12:11:05 +0300

    add solution level 37 task 06

---
+++ src/main/java/ru/javarush/java/core/level37/task06/Solution.java
@@ -0,0 +1,15 @@
+package ru.javarush.java.core.level37.task06;
+
+import java.nio.charset.Charset;
+import java.util.Map;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Получаем от JVM все доступные кодировки.
+        // Метод возвращает отсортированную карту: ключ — каноническое (официальное) имя, значение — объект Charset.
+
+
+        // Перебираем все элементы карты (все поддерживаемые JVM кодировки)
+
+    }
+}
