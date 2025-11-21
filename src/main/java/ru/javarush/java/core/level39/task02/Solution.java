e01f2a62001b23966da2bc1ab4aee3ab6807aad5
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-21 12:14:52 +0300

    add solution level 39 task 02

---
+++ src/main/java/ru/javarush/java/core/level39/task02/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level39.task02;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Задаем корневую директорию "src" через Path (требование задачи)
+
+
+        // Рекурсивно обходим все поддиректории с помощью Files.walk
+        // Используем try-with-resources, чтобы корректно закрыть поток путей
+
+    }
+}
