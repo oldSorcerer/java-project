4f9d185bd0f93aa86c1dd12aaa61089ab77d3a1e
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-21 12:13:40 +0300

    add solution level 38 task 07

---
+++ src/main/java/ru/javarush/java/core/level38/task07/Solution.java
@@ -0,0 +1,23 @@
+package ru.javarush.java.core.level38.task07;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        // Считываем путь к файлу как текстовую строку
+
+
+        // Создаем объект Path на основе введенной строки
+
+
+        // Проверяем право на чтение файла с помощью Files.isReadable(Path)
+
+
+        // Выводим результат проверки: true или false
+
+    }
+}
