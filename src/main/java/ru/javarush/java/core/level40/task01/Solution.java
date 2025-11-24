02ec7ca71191903ab12e319f2d875fec9780d681
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-24 08:52:17 +0300

    add solution level 40 task 01

---
+++ src/main/java/ru/javarush/java/core/level40/task01/Solution.java
@@ -0,0 +1,19 @@
+package ru.javarush.java.core.level40.task01;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        // Формируем относительный путь 'reports/2024/march' от текущей рабочей директории
+
+
+        // Создаем все недостающие директории одним методом.
+        // Если часть пути уже существует — исключения не будет.
+
+
+        // Выводим абсолютный путь к созданной (или уже существующей) папке 'march'
+
+    }
+}
