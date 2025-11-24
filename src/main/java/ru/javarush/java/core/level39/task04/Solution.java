32e3c3053aec8a755b5514a902c838e99e755a9a
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-24 14:53:16 +0300

    add solution level 39 task 04

---
+++ src/main/java/ru/javarush/java/core/level39/task04/Solution.java
@@ -0,0 +1,21 @@
+package ru.javarush.java.core.level39.task04;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        // Создаем путь к файлу "info.txt"
+
+
+        // Проверяем существование файла
+
+
+        // Получаем базовые атрибуты файла (создание, изменение и др.)
+
+
+        // Выводим требуемую информацию
+
+    }
+}
