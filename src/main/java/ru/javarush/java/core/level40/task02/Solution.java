8e8806662015805d5f8cbfda4805343d79935483
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-24 08:52:26 +0300

    add solution level 40 task 02

---
+++ src/main/java/ru/javarush/java/core/level40/task02/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level40.task02;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Определяем путь к файлу, который нужно удалить
+
+
+        // Сначала осторожно проверяем, существует ли файл
+
+    }
+}
