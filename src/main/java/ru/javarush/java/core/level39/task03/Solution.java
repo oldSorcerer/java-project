5598461d1a6fe9f593419da25041793c09564d63
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-24 14:53:11 +0300

    add solution level 39 task 03

---
+++ src/main/java/ru/javarush/java/core/level39/task03/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level39.task03;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект пути, указывающий на "example" относительно текущей директории
+
+
+        // Проверяем существование объекта по пути
+
+    }
+}
