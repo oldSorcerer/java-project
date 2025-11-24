40711a4112c9db266a08bf4ed53244edb428a155
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-24 08:53:40 +0300

    add solution level 41 task 01

---
+++ src/main/java/ru/javarush/java/core/level41/task01/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level41.task01;
+
+import java.io.FileInputStream;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        // Подсчёт общего количества байт
+        int byteCount = 0;
+
+        // Открываем файл для чтения с помощью FileInputStream
+        // try-with-resources гарантирует закрытие потока даже при исключениях
+
+
+        // Выводим результат в требуемом формате
+
+    }
+}
