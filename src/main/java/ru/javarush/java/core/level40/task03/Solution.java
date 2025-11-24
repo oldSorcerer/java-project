e6a7c816b135d76458aeb88e9cc19fb4bdc8de7e
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-24 08:52:33 +0300

    add solution level 40 task 03

---
+++ src/main/java/ru/javarush/java/core/level40/task03/Solution.java
@@ -0,0 +1,23 @@
+package ru.javarush.java.core.level40.task03;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        // Источник: файл data/report.txt
+
+        // Назначение: файл archive/report.txt (имя сохраняем тем же)
+
+
+        // Создаём папку archive, если она ещё не существует
+
+
+        // Перемещаем файл с заменой, если целевой уже существует
+
+
+        // Сообщение выводим только при успешном перемещении
+
+    }
+}
