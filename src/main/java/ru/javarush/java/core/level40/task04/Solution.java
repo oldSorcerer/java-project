fb2f063a136a3874c4a1d2043fbdd7c96e3792e9
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-24 08:52:40 +0300

    add solution level 40 task 04

---
+++ src/main/java/ru/javarush/java/core/level40/task04/Solution.java
@@ -0,0 +1,22 @@
+package ru.javarush.java.core.level40.task04;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        // Формируем путь к исходному файлу относительно рабочей директории
+
+
+        // Формируем путь к файлу с новым именем в той же папке (через resolveSibling)
+
+
+        // Переименование фактически реализовано как перемещение в тот же каталог с новым именем
+
+
+        // Сообщаем об успешном переименовании
+
+    }
+}
