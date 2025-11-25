4ae6fc5fa15cbbe33091a895c1cb7804f276fc36
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-25 11:40:48 +0300

    add solution level 48 task 04

---
+++ src/main/java/ru/javarush/java/core/level48/task04/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level48.task04;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Исходный список водных объектов
+        List<String> waterBodies = List.of("река", "море", "озеро", "ручей", "пруд");
+
+        // Используем Stream API:
+
+
+        // Выводим итоговый список на экран
+
+    }
+}
