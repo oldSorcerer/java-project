2b0912545b2d6a723a0ae5a6c94c065d16bc509e
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-25 11:41:21 +0300

    add solution level 48 task 08

---
+++ src/main/java/ru/javarush/java/core/level48/task08/Solution.java
@@ -0,0 +1,20 @@
+package ru.javarush.java.core.level48.task08;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем динамический список строк — нашу общую "доску мыслей"
+        List<String> words = new ArrayList<>();
+
+        // Лямбда-выражение типа Runnable.
+
+
+        // Запускаем действие
+
+
+        // Выводим содержимое списка. Ожидаемый результат: [Hello]
+
+    }
+}
