85eb65f94fb8ce2e4314eca6dabf654a4f867637
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-25 11:40:31 +0300

    add solution level 48 task 01

---
+++ src/main/java/ru/javarush/java/core/level48/task01/Solution.java
@@ -0,0 +1,22 @@
+package ru.javarush.java.core.level48.task01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Компаратор, который сравнивает строки по их длине (короче — "меньше")
+
+
+        // Создаем список строк
+        List<String> names = new ArrayList<>(Arrays.asList("cat", "elephant", "dog"));
+
+        // Сортируем список с использованием нашего компаратора
+
+
+        // Выводим отсортированный список
+
+    }
+}
