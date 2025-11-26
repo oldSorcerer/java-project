7c0d2eb448f8af75a01f1c59c39cc976f9220179
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-26 11:43:48 +0300

    add solution level 49 task 09

---
+++ src/main/java/ru/javarush/java/core/level49/task09/Solution.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level49.task09;
+
+import java.util.stream.IntStream;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Функциональный подход: используем IntStream для представления доходов и агрегируем их без циклов
+
+
+        // Выводим итоговую месячную выручку на экран
+
+    }
+}
