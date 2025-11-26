38fcd8e045e91a3fb9eaae60c343ff108446eb14
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-26 11:43:35 +0300

    add solution level 49 task 07

---
+++ src/main/java/ru/javarush/java/core/level49/task07/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level49.task07;
+
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Инвентарь представлен в виде коллекции строк (неизменяемый список)
+        List<String> inventory = List.of("кот", "слон", "носорог", "тигр", "мышь");
+
+        // Используем Stream API: проецируем строки в их длины и суммируем
+
+
+        // Вывод результата в требуемом формате
+
+    }
+}
