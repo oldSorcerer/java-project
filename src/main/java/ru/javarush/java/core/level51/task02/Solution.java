f58d28fa3dea31bb19828179c53b27f358d44261
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-26 11:46:20 +0300

    add solution level 51 task 02

---
+++ src/main/java/ru/javarush/java/core/level51/task02/Solution.java
@@ -0,0 +1,11 @@
+package ru.javarush.java.core.level51.task02;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Вывод числовых маркеров: 1, 2, 3
+
+
+        // Вывод буквенных маркеров: A, B, C
+
+    }
+}
