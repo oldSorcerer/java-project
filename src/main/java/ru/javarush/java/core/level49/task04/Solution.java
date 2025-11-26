ce98f84b92fc4d59541d26d4a737b2ac11e54e6d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-26 11:43:12 +0300

    add solution level 49 task 04

---
+++ src/main/java/ru/javarush/java/core/level49/task04/Solution.java
@@ -0,0 +1,26 @@
+package ru.javarush.java.core.level49.task04;
+
+import java.util.function.Function;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Функция 1: преобразует строку в целое число
+
+
+        // Функция 2: возводит число в квадрат
+
+
+        // Функция 3: формирует итоговую строку с префиксом "Result: "
+
+
+        // Композиция функций: String -> Integer -> Integer -> String
+        // Используем andThen: результат предыдущей функции передается следующей
+
+
+        // Альтернатива через compose (эквивалентная цепочка):
+
+
+        // Тестируем на числе "6"
+
+    }
+}
