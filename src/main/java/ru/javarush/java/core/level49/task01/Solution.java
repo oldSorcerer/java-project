a6d580c8011d6e68532ea4d6a2d19b6ee3e78b67
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-26 11:42:55 +0300

    add solution level 49 task 01

---
+++ src/main/java/ru/javarush/java/core/level49/task01/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level49.task01;
+
+import java.util.function.Function;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Function принимает любую строку и возвращает её длину
+
+
+        // Тестовые заголовки
+        String[] titles = {"Stream", "Java", "Predicate"};
+
+        // Выводим результаты в требуемом формате
+
+    }
+}
