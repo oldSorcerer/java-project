0e6b84ce93c1d03b348db3f0cf4055d0fe58cd35
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-26 11:43:56 +0300

    add solution level 49 task 10

---
+++ src/main/java/ru/javarush/java/core/level49/task10/Solution.java
@@ -0,0 +1,25 @@
+package ru.javarush.java.core.level49.task10;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Исходный список сообщений: среди них есть null и пустые строки
+        String[] messages = {
+                "Привет", null, "",
+                "Как дела?",
+                "java", "",
+                "Полёт нормальный",
+                null
+        };
+
+        // Последовательная обработка через Stream:
+
+
+        // Выводим очищенные и отформатированные сообщения на экран (по одному в строке)
+
+    }
+}
