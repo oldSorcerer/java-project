e385dbf40e890ffd84942f0ee5c22c49985f5806
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-26 11:43:27 +0300

    add solution level 49 task 06

---
+++ src/main/java/ru/javarush/java/core/level49/task06/Solution.java
@@ -0,0 +1,27 @@
+package ru.javarush.java.core.level49.task06;
+
+import java.util.List;
+import java.util.function.Function;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Исходный список числовых показателей
+        List<Integer> intensities = List.of(2, 4, 6, 8);
+
+        // Функция-преобразователь: число -> строка с подписью
+
+
+        // Передаём функцию как параметр в метод преобразования
+
+
+        // Выводим получившийся список строк на экран
+
+    }
+
+    // Метод принимает список чисел и функцию преобразования.
+    // Здесь явно демонстрируется "передача функции как параметра".
+    private static List<String> transform( ... ) {
+        // Современный способ обработки данных: Stream API
+
+    }
+}
