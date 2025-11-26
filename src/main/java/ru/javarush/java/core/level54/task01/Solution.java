dbd2ade7587e55c57f99180eab28b01f714bed18
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-26 12:01:23 +0300

    add solution level 54 task 01

---
+++ src/main/java/ru/javarush/java/core/level54/task01/Solution.java
@@ -0,0 +1,15 @@
+package ru.javarush.java.core.level54.task01;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем новый поток и сразу присваиваем ему имя "Worker-Thread"
+
+            // Внутри потока получаем его собственное имя стандартным способом
+
+            // Выводим имя на экран
+
+
+        // Запускаем поток
+
+    }
+}
