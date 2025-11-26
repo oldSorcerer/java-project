d3071af8be950a7c5271be5d748478ecfbc79b67
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-26 11:45:21 +0300

    add solution level 50 task 06

---
+++ src/main/java/ru/javarush/java/core/level50/task06/MagicalPulseListener.java
@@ -0,0 +1,9 @@
+package ru.javarush.java.core.level50.task06;
+
+import java.util.EventListener;
+
+// Интерфейс для обработки магических импульсов
+public interface MagicalPulseListener  {
+    // Метод, который вызывается при получении магического импульса
+
+}
---
+++ src/main/java/ru/javarush/java/core/level50/task06/SimpleMageListener.java
@@ -0,0 +1,10 @@
+package ru.javarush.java.core.level50.task06;
+
+// Класс, реализующий интерфейс MagicalPulseListener
+public class SimpleMageListener  {
+
+    // Реализация метода для обработки магического импульса
+
+        // Выводим сообщение о получении импульса в консоль
+
+}
