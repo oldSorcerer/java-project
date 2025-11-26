16c410703076fa5d4654aa773dff0ae0293bfd44
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-26 11:43:42 +0300

    add solution level 49 task 08

---
+++ src/main/java/ru/javarush/java/core/level49/task08/Solution.java
@@ -0,0 +1,18 @@
+package ru.javarush.java.core.level49.task08;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Исходный список названий городов
+        List<String> cities = List.of("Минск", "Тверь", "Калуга", "Орел", "Тула", "Рязань");
+
+        // Конвейер Stream API:
+
+
+        // Выводим итоговый список
+
+    }
+}
