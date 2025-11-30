85f5250813b67b4e44aea546ae251e634a879a87
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-30 17:45:06 +0300

    add solution level 55 task 04

---
+++ src/main/java/ru/javarush/java/core/level55/task04/Solution.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level55.task04;
+
+import java.util.concurrent.CompletableFuture;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Строим цепочку асинхронных действий:
+
+
+        // Ждем завершения всей цепочки, чтобы программа не завершилась раньше времени
+
+    }
+}
