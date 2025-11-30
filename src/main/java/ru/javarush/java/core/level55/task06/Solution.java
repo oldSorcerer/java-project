b0efa8c2f4755867cf90dca62081983912fe0ffe
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-30 17:45:18 +0300

    add solution level 55 task 06

---
+++ src/main/java/ru/javarush/java/core/level55/task06/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level55.task06;
+
+import java.util.concurrent.CompletableFuture;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Первая асинхронная задача: возвращает начальную часть приветствия
+
+
+        // Вторая асинхронная задача: возвращает вторую часть приветствия
+
+
+        // thenCombine дождется завершения обеих задач и "склеит" их результаты
+
+    }
+}
