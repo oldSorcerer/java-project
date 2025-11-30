dbf5a31a5a86f74c40405177f891cb07788b3dd8
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-30 17:45:58 +0300

    add solution level 56 task 01

---
+++ src/main/java/ru/javarush/java/core/level56/task01/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level56.task01;
+
+import java.io.IOException;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        // Готовим путь к файлу-источнику
+        Path path = Path.of("example.txt");
+
+        // Открываем канал исключительно для чтения (READ)
+
+    }
+}
