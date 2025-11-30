18129be3bd6724293a153eaf3c6d84966d34cc8e
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-30 17:43:59 +0300

    add solution level 54 task 06

---
+++ src/main/java/ru/javarush/java/core/level54/task06/Solution.java
@@ -0,0 +1,23 @@
+package ru.javarush.java.core.level54.task06;
+
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Исходный список участников парада
+        List<String> animals = List.of("cat", "dog", "elephant", "fox", "lion", "tiger", "wolf");
+
+        // Параллельно сортируем животных по длине имени
+
+
+        // Группируем по длине имени, сохраняя порядок групп так, как он встречается в уже отсортированном списке
+
+        
+        // Выводим итог: сгруппированный и отсортированный список
+
+    }
+}
