0e5a4f64b44350e6122370a1202bb09081f67796
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-30 17:45:23 +0300

    add solution level 55 task 07

---
+++ src/main/java/ru/javarush/java/core/level55/task07/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level55.task07;
+
+import java.util.concurrent.CompletableFuture;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Стартуем "сенсор" асинхронно
+
+                // В случае ошибки возвращаем безопасное значение 0
+
+                // Независимо от исхода: удваиваем полученное значение
+
+
+        // Дожидаемся завершения асинхронной цепочки и выводим итоговый результат
+
+    }
+}
