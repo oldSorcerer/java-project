0444df38abe3a352774285a8fda0f001ff11f9b3
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-11-30 17:47:28 +0300

    add solution level 57 task 01

---
+++ src/main/java/ru/javarush/java/core/level57/task01/Solution.java
@@ -0,0 +1,12 @@
+package ru.javarush.java.core.level57.task01;
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        // Запускаем "виртуальный мессенджер" как виртуальный поток (Java 21+)
+
+
+
+        // Терпеливо ждём завершения работы виртуального потока
+
+    }
+}
