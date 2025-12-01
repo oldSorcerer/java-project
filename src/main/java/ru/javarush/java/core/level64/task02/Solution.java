2841dedcbb1194c1267fdf1ffe501a4dc1071574
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-12-01 23:29:02 +0300

    add solution level 64 task 02

---
+++ src/main/java/ru/javarush/java/core/level64/task02/Solution.java
@@ -0,0 +1,20 @@
+package ru.javarush.java.core.level64.task02;
+
+import java.util.ArrayList;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Коллекция ArrayList хранит ссылки на массивы,
+        // чтобы сборщик мусора не мог их освободить
+
+
+        // размер одного массива (>= 1_000_000)
+
+        
+        // Бесконечный цикл: постоянно добавляем новые большие массивы в список
+
+    }
+}
+/*
+Ответ: ...
+*/
