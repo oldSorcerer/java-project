ad98b3b721b87734ce7dc1e2d7f569f3daa97c0e
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-12-01 23:26:03 +0300

    add solution level 62 task 01

---
+++ src/main/java/ru/javarush/java/core/level62/task01/Solution.java
@@ -0,0 +1,11 @@
+package ru.javarush.java.core.level62.task01;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Получаем объект класса Class, используя синтаксис .class для типа String
+
+
+        // Выводим полное имя класса (должно быть "java.lang.String")
+
+    }
+}
