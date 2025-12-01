cde1642dbb87f25190e27dc4ae9b368c2b7f543c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-12-01 22:53:25 +0300

    add solution level 61 task 01

---
+++ src/main/java/ru/javarush/java/core/level61/task01/Solution.java
@@ -0,0 +1,15 @@
+package ru.javarush.java.core.level61.task01;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Получаем имя текущей ОС из системных свойств JVM
+        // Пример значения: "Windows 10", "Linux", "Mac OS X"
+
+
+        // Проверяем, является ли ОС Windows (учитываем, что в имени могут быть дополнительные слова)
+
+
+        // Выводим результат в требуемом формате
+
+    }
+}
