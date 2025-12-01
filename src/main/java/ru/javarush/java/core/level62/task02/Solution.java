9aa00ca1614f74f7bda4d2a2135c0a6f62d9dcaa
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-12-01 23:26:12 +0300

    add solution level 62 task 02

---
+++ src/main/java/ru/javarush/java/core/level62/task02/Solution.java
@@ -0,0 +1,20 @@
+package ru.javarush.java.core.level62.task02;
+
+import java.io.IOException;
+
+public class Solution {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        // Полное имя класса, который хотим загрузить динамически
+        String className = "java.util.ArrayList";
+
+        // Динамическая загрузка класса по имени
+
+
+        // Получаем пакет класса и его имя
+
+
+        // Выводим имя пакета на экран
+
+
+    }
+}
