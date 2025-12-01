f84911802f1f30e31f984e1dbe8fb39c1aab75ee
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-12-01 23:27:41 +0300

    add solution level 63 task 04

---
+++ src/main/java/ru/javarush/java/core/level63/task04/Solution.java
@@ -0,0 +1,18 @@
+package ru.javarush.java.core.level63.task04;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Solution {
+    // Инициализируем логгер для текущего класса с помощью SLF4J
+
+
+    public static void main(String[] args) {
+        // Объявляем две переменные: имя пользователя и описание действия
+        String loggedInUser = "Alice";                // произвольное имя пользователя
+        String performedAction = "опубликовал пост";   // произвольное описание действия
+
+        // Логируем сообщение на уровне INFO, используя шаблон с автоматической подстановкой переменных
+
+    }
+}
