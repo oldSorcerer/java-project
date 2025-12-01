f62aa9e08eeca7bc7c049ec2c4904f68dff00a34
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-12-01 23:28:50 +0300

    add solution level 64 task 01

---
+++ src/main/java/ru/javarush/java/core/level64/task01/Solution.java
@@ -0,0 +1,11 @@
+package ru.javarush.java.core.level64.task01;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Запускаем бесконечную рекурсию — "заклинание" без остановки
+    
+    }
+
+    // Рекурсивный метод, который вызывает сам себя без условия выхода.
+
+}
