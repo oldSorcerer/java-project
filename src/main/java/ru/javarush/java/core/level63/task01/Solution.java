c6d333d96c16327e60c46192601af0f8e97f19d3
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-12-01 23:27:26 +0300

    add solution level 63 task 01

---
+++ src/main/java/ru/javarush/java/core/level63/task01/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level63.task01;
+
+import java.util.logging.Logger;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Получаем логгер через фабричный метод Logger.getLogger()
+
+
+        // Логируем предупреждение
+
+
+        // Логируем критическую ошибку
+
+    }
+}
