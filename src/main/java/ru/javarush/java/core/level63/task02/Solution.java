ae82eb617c6d24733dff5424c61fc4b2082e9b10
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-12-01 23:27:32 +0300

    add solution level 63 task 02

---
+++ src/main/java/ru/javarush/java/core/level63/task02/Solution.java
@@ -0,0 +1,20 @@
+package ru.javarush.java.core.level63.task02;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Solution {
+    // Получение логгера для текущего класса через SLF4J
+
+
+    public static void main(String[] args) {
+        // Вывод информационного сообщения об успешной инициализации
+
+        
+        // Вывод предупреждающего сообщения об обнаруженной проблеме
+
+        
+        // Вывод сообщения об ошибке при серьезном сбое
+
+    }
+}
