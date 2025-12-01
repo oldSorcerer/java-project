4a689750cd2d398e9e78a3fcf9209bed9b0f0032
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-12-01 23:29:39 +0300

    add solution level 64 task 09

---
+++ src/main/java/ru/javarush/java/core/level64/task09/Solution.java
@@ -0,0 +1,19 @@
+package ru.javarush.java.core.level64.task09;
+
+import java.io.FileInputStream;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        // Открываем текстовый файл для чтения через FileInputStream
+
+
+        // Считываем только первый байт (если файл пустой — вернется -1)
+
+
+        // Выводим значение прочитанного байта на экран
+
+
+        // Это демонстрация утечки ресурсов
+
+    }
+}
