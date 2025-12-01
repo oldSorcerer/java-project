01b0dbf1e84fc95feb51ef13535523cf275dae1d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-12-01 23:27:36 +0300

    add solution level 63 task 03

---
+++ src/main/java/ru/javarush/java/core/level63/task03/Solution.java
@@ -0,0 +1,19 @@
+package ru.javarush.java.core.level63.task03;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Solution {
+    // Формат даты и времени с миллисекундами: "yyyy-MM-dd HH:mm:ss.SSS"
+
+
+    // Универсальный метод форматирования лога
+    public static String formatLog(LocalDateTime time, String level, String source, String message) {
+
+    }
+
+    public static void main(String[] args) {
+        // Демонстрация соответствия примеру из условия с фиксированным временем:
+
+    }
+}
